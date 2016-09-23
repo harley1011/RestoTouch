7ebeef2d57ddb63b6f7ade9ee16c1acbc209b276
harley1011 <harley.1011@gmail.com>
2016-09-23 16:57:23 -0400

    Removing src from gitignore, shouldn't of added it

---
+++ src/client/app/dashboard/blank-page/blankPage.routes.js
@@ -0,0 +1,9 @@
+"use strict";
+var index_1 = require('./index');
+exports.BlankPageRoutes = [
+    {
+        path: 'blankpage',
+        component: index_1.BlankPageComponent
+    }
+];
+//# sourceMappingURL=blankPage.routes.js.map
