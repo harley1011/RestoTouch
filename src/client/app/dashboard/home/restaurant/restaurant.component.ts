714ff8b4e527a3b152eb2ea61dc2187be7026c6a
davidbastien5 <david.bastien5@gmail.com>
2016-09-25 15:54:04 -0400

    Added angular js ngFor to auto populate the array of restaurants onto the dashboard

--- src/client/app/dashboard/home/restaurant/restaurant.component.ts
+++ src/client/app/dashboard/home/restaurant/restaurant.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Restaurant {
+  name: string;
+  description: string;
+}
+
+const RESTAURANTS: Restaurant[] = [
+  {name: 'McDonald', description: 'Fast food restaurant'},
+  {name: 'Yuan Vegetarien', description: 'Asian vegan restaurant'},
+  {name: 'Copper Branch', description: 'Vegan fast food restaurant'}
+];
+
 @Component({
 	moduleId: module.id,
 	selector: 'restaurant-cmp',
 	templateUrl: 'restaurant.component.html'
 })
-export class RestaurantComponent { }
+export class RestaurantComponent {
+  restaurants = RESTAURANTS;
+}
