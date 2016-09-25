ac9343ed6ef7acdbdfad0d73d131d822c699b80a
davidbastien5 <david.bastien5@gmail.com>
2016-09-25 15:18:04 -0400

    Cleaned dashboard and added files for restaurant adding/modifying/deleting component #1

---
+++ src/client/app/dashboard/home/restaurant/restaurant.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'restaurant-cmp',
+	templateUrl: 'restaurant.component.html'
+})
+export class RestaurantComponent { }
