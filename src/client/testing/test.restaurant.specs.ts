fbad658c3418c6cfe547791c6e2263ba3d883d52
Samer El-Achkar <samerelachkarmail@gmail.com>
2016-09-28 18:03:39 -0400

    Unit test for restaurant #1

---
+++ src/client/testing/test.restaurant.specs.ts
@@ -0,0 +1,24 @@
+//import { RestaurantService } from './restaurant.service';
+
+export function main() {
+  describe('create, modify and remove virtual restaurants', () => {
+
+    beforeEach(() => {
+    });
+
+    it('creating the restaurant', function() => {
+      expect(2).toEqual(1);//testing to see if karma shows me it fails
+    });
+
+    it('modifying the restaurant', function() => {
+
+    });
+
+    it('removing the restaurant', function() => {
+
+    });
+
+  });
+
+
+}
