0dda2ff63701d97abae671fbdbaa42acb3172edc
Samer El-Achkar <samerelachkarmail@gmail.com>
2016-09-28 18:06:06 -0400

    Unit test for register #6

---
+++ src/client/testing/test.signup.specs.ts
@@ -0,0 +1,17 @@
+export function main() {
+  describe('registering into the website', () => {
+
+    beforeEach(() => {
+    });
+
+    it('registered after completing form correctly', function() => {
+      expect(2).toEqual(1);//testing to see if kama shows me it fails
+    });
+
+    it('unregistered after wrong input', function() => {
+
+    });
+
+  });
+
+}
