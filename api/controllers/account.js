e83c072ed6b5929d0804fbcd622895c699314f7a
harley1011 <harley.1011@gmail.com>
2016-09-29 16:51:49 -0400

    Hashing passwords stored in database for registration #6

--- api/controllers/account.js
+++ api/controllers/account.js
@@ -1,4 +1,5 @@
 var models = require("../../database/models");
+var passwordHasher = require("../helpers/password_hash");
 var userModel = models.getUserModel();
 
 module.exports = {
@@ -8,8 +9,13 @@ module.exports = {
 
 function register(req, res) {
   var user = req.body;
+  var passwordData = passwordHasher.saltHashPassword(user.password);
+  user.password = passwordData.passwordHash;
+  user.salt = passwordData.salt;
 
   userModel.create(user).then(function(result) {
     return res.json({success: 1, description: "User registered"});
   });
 }
+
+
