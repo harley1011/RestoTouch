2bd9b2fd39100768dda8d6de9f005febda3a0c6d
harley1011 <harley.1011@gmail.com>
2016-09-29 16:19:30 -0400

    Adding database insert for register api path #6

--- api/controllers/account.js
+++ api/controllers/account.js
@@ -1,11 +1,15 @@
+var models = require("../../database/models");
+var userModel = models.getUserModel();
+
 module.exports = {
   register: register
 };
 
 
 function register(req, res) {
-  var params = req.swagger.params;
-console.log("hkh")
-  // this sends back a JSON response which is a single string
-  res.json({success: true});
+  var user = req.body;
+
+  userModel.create(user).then(function(result) {
+    return res.json({success: 1, description: "User registered"});
+  });
 }
