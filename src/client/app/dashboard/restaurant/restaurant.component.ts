f424335aa4aaa2c3ef4b5e5147a0e64c965e1f5c
davidbastien5 <david.bastien5@gmail.com>
2016-09-30 15:06:02 -0400

    Fixing small error #1

--- src/client/app/dashboard/restaurant/restaurant.component.ts
+++ src/client/app/dashboard/restaurant/restaurant.component.ts
@@ -81,7 +81,7 @@ export class RestaurantComponent implements OnInit {
 					this.router.navigate(['/dashboard/home']);
 				},
 				error => {
-					this.errorMessage = <any>error
+					this.errorMessage = <any>error;
 				}
 			);
 	}
