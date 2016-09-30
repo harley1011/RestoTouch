d24ee82e4a85f2a4780d1bbcf1a9de8310c7a203
David Bastien <david.bastien5@gmail.com>
2016-09-30 12:42:27 -0400

    Done input gathering and validation #1

--- src/client/app/dashboard/restaurant/restaurant.service.ts
+++ src/client/app/dashboard/restaurant/restaurant.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 
 import { Restaurant } from '../home/restaurantlist/restaurant';
+import { GeneralResponse }  from '../../shared/general.response';
 
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 
@@ -23,17 +24,36 @@ export class RestaurantService {
 
   getRestaurant (): Observable<Restaurant[]> {
     return this.http.get('http://localhost:10010/restaurant')
-      .map(this.extractData)
+      .map(this.extractRestaurantData)
       .catch(this.handleError);
   }
 
-  private extractData(res: Response) {
+  addRestaurant (restaurant: Restaurant): Observable<GeneralResponse> {
+    let body = JSON.stringify(restaurant);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post('http://localhost:10010/restaurant', body, options)
+      .map(this.extractGeneralResponseData)
+      .catch(this.handleError);
+  }
+
+  private extractRestaurantData(res: Response) {
     let body = res.json();
     return body.restaurants || { };
   }
 
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
+  private extractGeneralResponseData(res: Response) {
+    let body = res.json();
+    return body || { };
+  }
+
+  private handleError (error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
   }
 }
