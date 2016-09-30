b7e99e95c7de5ab6fcddf4b36622082bec7a0bbd
harley1011 <harley.1011@gmail.com>
2016-09-29 20:25:13 -0400

    Adding user.service for handling HTTP api register call and adding logic to signup.component to use service #6

--- src/client/app/signup/signup.component.ts
+++ src/client/app/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { User } from './user';
-
+import { UserService }       from './user.service';
 /**
 *	This class represents the lazy loaded SignupComponent.
 */
@@ -9,7 +9,22 @@ import { User } from './user';
 @Component({
 	moduleId: module.id,
 	selector: 'signup-cmp',
-	templateUrl: 'signup.component.html'
+	templateUrl: 'signup.component.html',
+  providers: [ UserService ]
 })
 
-export class SignupComponent { user = new User('', '', ''); }
+export class SignupComponent {
+  user = new User('', '', '', '', '');
+  errorMessage: string;
+
+  constructor (private userService: UserService) {}
+
+  onSubmit() {
+    this.userService.registerUser(this.user)
+                      .subscribe(
+                        user => this.user = user,
+                        error =>  this.errorMessage = <any>error);
+
+  }
+
+}
