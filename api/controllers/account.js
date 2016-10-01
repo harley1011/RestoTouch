2d6212fbf983daff2fd99d2e9b90514c24209532
harley1011 <harley.10111@gmail.com>
2016-10-01 09:58:06 -0400

    Adding jasmine tests for register and adding mock module to mock sequelize db operation #6

--- api/controllers/account.js
+++ api/controllers/account.js
@@ -1,22 +1,27 @@
 var models = require("../../database/models");
 var passwordHasher = require("../helpers/password_hash");
-var userModel = models.getUserModel();
+var userModel;
 var jwt = require('jwt-simple');
 var configAuth = require('../../config/auth');
 
+
+
 module.exports = {
-  register: register
+  register: register,
+  setDatabase: setDatabase
 };
 
+function setDatabase (m) {
+  models = m;
+  userModel = models.getUserModel();
+}
 
 function register(req, res) {
   var user = req.body;
   var passwordData = passwordHasher.saltHashPassword(user.password);
   user.password = passwordData.passwordHash;
   user.salt = passwordData.salt;
-
-  userModel.create(user).then(function(newUser) {
-
+  return userModel.create(user).then(function(newUser) {
     var info = userInfo(newUser.dataValues);
     return res.json({success: 1, description: "User registered", "user": info.user, "accessToken":  info.token});
   });
--- api/spec/account.spec.js
+++ api/spec/account.spec.js
@@ -1,5 +1,32 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
+var account = require('../controllers/account');
+var mocks = require('./mocks');
+
+
+
+describe("Register Tests", function() {
+
+  var req = {
+    body: {
+      "firstName": "Harley",
+      "lastName": "McPhee",
+      "email": "test@gmail.com",
+      "password": "Password"
+    }
+  }
+
+  var res = {json: function (obj) {
+    res.obj = obj;
+  }};
+
+  //Override models in account so that we don't actually insert into the DB and we mock the database operations
+  account.setDatabase(mocks);
+
+  it("Register a valid user", function (done) {
+    account.register(req, res).then(function (result) {
+      expect(res.obj.user.id).toBe(100);
+      expect(res.obj.user.email).toBe(req.body.email);
+      done();
+    })
+  })
+
 });
