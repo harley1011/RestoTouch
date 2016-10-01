448b9c7dadcd448b3d56bc0b0e5ffce8d4493c24
Samer El-Achkar <samerelachkarmail@gmail.com>
2016-09-30 23:56:49 -0400

    built prod for first time

---
+++ dist/prod/app/dashboard/forms/index.js
@@ -0,0 +1,10 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+/**
+ * This barrel file provides the export for the lazy loaded FormsComponent.
+ */
+__export(require('./forms.component'));
+__export(require('./forms.routes'));
+//# sourceMappingURL=index.js.map
