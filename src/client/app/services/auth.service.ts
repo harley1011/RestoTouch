367ecb57ae83535327d413eb4c5ce4493efec80c
harley1011 <harley.1011@gmail.com>
2016-10-01 16:50:35 -0400

    Refactoring auth service #7

--- src/client/app/services/auth.service.ts
+++ src/client/app/services/auth.service.ts
@@ -3,17 +3,38 @@ import { Injectable }     from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
 
-import { Auth }           from './auth';
+import { User } from '../shared/models/user';
 import { Observable }     from 'rxjs/Observable';
 import { GeneralResponse }  from '../shared/general.response';
 
 @Injectable()
 export class AuthService {
   private url = 'http://localhost:10010/login';
+  private loggedIn: boolean;
+  private loggedInUser: User;
 
   constructor (private http: Http) {}
 
-  authenticateUser (user: Auth): Observable<GeneralResponse> {
+  isLoggedIn () : Boolean {
+    return this.loggedIn;
+  }
+
+  loggedInUser(): User {
+    return this.loggedInUser;
+  }
+
+  authenticateUser (user: User): Observable<GeneralResponse> {
+    let body = JSON.stringify(user);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.url, body, options)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+
+  registerUser (user: User): Observable<GeneralResponse> {
     let body = JSON.stringify(user);
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
@@ -26,12 +47,13 @@ export class AuthService {
   private extractData(res: Response) {
     let body = res.json();
     localStorage.setItem('authToken', body.accessToken);
+    localStorage.setItem('user', body.user);
+    this.loggedInUser = new User(body.user.firstName, body.user.lastName, body.user.email);
+    this.loggedIn = true;
     return body || { };
   }
 
   private handleError (error: any) {
-    // In a real world app, we might use a remote logging infrastructure
-    // We'd also dig deeper into the error to get a better message
     let errMsg = (error.message) ? error.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
     console.error(errMsg); // log to console instead
