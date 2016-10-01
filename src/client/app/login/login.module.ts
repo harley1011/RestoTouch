81ddc1ead646dc6e21c3b0c450c29bcf1fdb808b
Hilary Chan <hilarychan079@gmail.com>
2016-10-01 12:50:47 -0400

    fixed login background #7

--- src/client/app/login/login.module.ts
+++ src/client/app/login/login.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule, RouterModule],
-    declarations: [LoginComponent],
-    exports: [LoginComponent]
+   imports: [CommonModule, RouterModule, FormsModule],
+   declarations: [LoginComponent],
+   exports: [LoginComponent]
 })
-
 export class LoginModule { }
