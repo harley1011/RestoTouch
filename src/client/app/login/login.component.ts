bc095403802ba03e816c1a47495278b68e79d0aa
harley1011 <harley.1011@gmail.com>
2016-10-01 15:26:24 -0400

    Removing unused variable in login.component #7

--- src/client/app/login/login.component.ts
+++ src/client/app/login/login.component.ts
@@ -25,7 +25,6 @@ export class LoginComponent {
 	onSubmit() {
 		this.authService.authenticateUser(this.user)
                         .subscribe(generalResponse => {
-				            console.log(loginResponse);
                             this.router.navigate(['/dashboard/home']);
 				        }, error =>  this.errorMessage = <any>error);
     }
