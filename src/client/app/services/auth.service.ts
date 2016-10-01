3ad755d073cae19df5f4a3ce3ec5afd232506e81
harley1011 <harley.10111@gmail.com>
2016-10-01 17:38:39 -0400

    Working on storing api key #7

--- src/client/app/services/auth.service.ts
+++ src/client/app/services/auth.service.ts
@@ -1,43 +1,43 @@
 // Observable Version
-import { Injectable }     from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Headers, RequestOptions } from '@angular/http';
+import {Injectable}     from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Headers, RequestOptions} from '@angular/http';
 
-import { User } from '../shared/models/user';
-import { Observable }     from 'rxjs/Observable';
-import { GeneralResponse }  from '../shared/general.response';
+import {User} from '../shared/models/user';
+import {Observable}     from 'rxjs/Observable';
+import {GeneralResponse}  from '../shared/general.response';
 
 @Injectable()
-export class AuthService {
+export class AuthService  {
+  public loggedInUser: User;
+
   private url = 'http://localhost:10010/login';
-  private loggedIn: boolean;
-  private loggedInUser: User;
 
-  constructor (private http: Http) {}
-
-  isLoggedIn () : Boolean {
-    return this.loggedIn;
+  constructor(private http: Http) {
+    var data = localStorage.getItem('user');
+    if (data !== null) {
+      this.loggedInUser = JSON.parse(data);
+    }
   }
 
-  loggedInUser(): User {
-    return this.loggedInUser;
+  isLoggedIn(): Boolean {
+    return this.loggedInUser !== null;
   }
 
-  authenticateUser (user: User): Observable<GeneralResponse> {
+  authenticateUser(user: User): Observable<GeneralResponse> {
     let body = JSON.stringify(user);
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
 
     return this.http.post(this.url, body, options)
       .map(this.extractData)
       .catch(this.handleError);
   }
 
-
-  registerUser (user: User): Observable<GeneralResponse> {
+  registerUser(user: User): Observable<GeneralResponse> {
     let body = JSON.stringify(user);
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
 
     return this.http.post(this.url, body, options)
       .map(this.extractData)
@@ -47,13 +47,12 @@ export class AuthService {
   private extractData(res: Response) {
     let body = res.json();
     localStorage.setItem('authToken', body.accessToken);
-    localStorage.setItem('user', body.user);
+    localStorage.setItem('user', JSON.stringify(body.user));
     this.loggedInUser = new User(body.user.firstName, body.user.lastName, body.user.email);
-    this.loggedIn = true;
-    return body || { };
+    return body || {};
   }
 
-  private handleError (error: any) {
+  private handleError(error: any) {
     let errMsg = (error.message) ? error.message :
       error.status ? `${error.status} - ${error.statusText}` : 'Server error';
     console.error(errMsg); // log to console instead
