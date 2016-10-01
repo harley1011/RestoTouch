b0ec840c9d9de7ba291138ced7d9a60f5a53fac8
Hilary Chan <hilarychan079@gmail.com>
2016-10-01 16:24:46 -0400

    validate hashed password #7

--- src/client/app/login/login.component.ts
+++ src/client/app/login/login.component.ts
@@ -25,6 +25,7 @@ export class LoginComponent {
 	onSubmit() {
 		this.authService.authenticateUser(this.user)
                         .subscribe(generalResponse => {
+				            console.log(generalResponse);
                             this.router.navigate(['/dashboard/home']);
 				        }, error =>  this.errorMessage = <any>error);
     }
