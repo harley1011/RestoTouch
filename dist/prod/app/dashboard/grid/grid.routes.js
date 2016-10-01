8ea20229125b1666f4faaceb0bc243433adaf1f8
Samer El-Achkar <samerelachkarmail@gmail.com>
2016-09-30 23:56:49 -0400

    built prod for first time

---
+++ dist/prod/app/dashboard/grid/grid.routes.js
@@ -0,0 +1,9 @@
+"use strict";
+var index_1 = require('./index');
+exports.GridRoutes = [
+    {
+        path: 'grid',
+        component: index_1.GridComponent
+    },
+];
+//# sourceMappingURL=grid.routes.js.map
---
+++ dist/prod/app/login/login.routes.js
@@ -0,0 +1,9 @@
+"use strict";
+var index_1 = require('./index');
+exports.LoginRoutes = [
+    {
+        path: '',
+        component: index_1.LoginComponent
+    }
+];
+//# sourceMappingURL=login.routes.js.map
