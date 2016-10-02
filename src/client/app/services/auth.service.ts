fb6ff4d312e7a3dd6552a91739dcd8d7a4f1c56c
harley1011 <harley.10111@gmail.com>
2016-10-02 00:57:57 -0400

    Fixing error reporting for login.component

--- src/client/app/services/auth.service.ts
+++ src/client/app/services/auth.service.ts
@@ -7,6 +7,7 @@ import {Router, CanActivate, CanActivateChild} from '@angular/router';
 import {User} from '../shared/models/user';
 import {Observable}     from 'rxjs/Observable';
 import {GeneralResponse}  from '../shared/general.response';
+import 'rxjs/Rx';
 
 @Injectable()
 export class AuthService implements CanActivate, CanActivateChild {
