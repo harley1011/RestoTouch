e3b42d853e9d7300fa0c5d94747ecfa4924724a8
Hilary Chan <hilarychan079@gmail.com>
2016-10-01 20:51:32 -0400

    added backend test for login #7

--- api/spec/mocks/mocks.js
+++ api/spec/mocks/mocks.js
@@ -2,6 +2,8 @@ var userModel = {}
 var restaurantModel = {}
 var Promise = require('promise');
 userModel.create = create;
+userModel.findOne = findOne_user;
+
 restaurantModel.create = create;
 restaurantModel.update = update;
 restaurantModel.findAll = findAll;
@@ -62,6 +64,17 @@ function findOne(options) {
     fulfill(object);
   });
 }
+
+function findOne_user(object) {
+    return new Promise(function (fulfill, reject) {
+      object.id = 100;
+      object.email = "test123@sample.com";
+      object.salt = 'salt';
+      object.password = "1c8e432462648d825ade4983da4b1c9cc231180d3dd0e77b0cfe0b28c5e2f2b39aa3adabfcd5e1fe968b9e815005cf67499c30177f4c0199e39064ceaa5adefa";
+      fulfill(object);
+    });
+}
+
 
 function destroy(options) {
   return new Promise(function (fulfill, reject) {
