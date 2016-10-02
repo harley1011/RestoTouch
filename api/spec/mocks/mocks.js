310ca92db578c6d3f70868d1157f9c70f1fc53c7
Hilary Chan <hilarychan079@gmail.com>
2016-10-01 21:06:28 -0400

    fixed login tests

--- api/spec/mocks/mocks.js
+++ api/spec/mocks/mocks.js
@@ -1,22 +1,71 @@
 var userModel = {}
+var restaurantModel = {}
 var Promise = require('promise');
 userModel.create = create;
-userModel.findOne = findOne;
+userModel.findOne = findOne_user;
+
+restaurantModel.create = create;
+restaurantModel.update = update;
+restaurantModel.findAll = findAll;
+restaurantModel.findOne = findOne;
+restaurantModel.destroy = destroy;
 
 
 exports.getUserModel = function() {
   return userModel;
 };
 
-
-function create(object) {
-  return new Promise(function (fulfill, reject){
-      object.id = 100;
-      fulfill({dataValues: object});
-    });
+exports.getRestaurantModel = function () {
+  return restaurantModel;
 }
 
-function findOne(object) {
+function create(object) {
+  return new Promise(function (fulfill, reject) {
+    object.id = 100;
+    fulfill({dataValues: object});
+  });
+}
+
+function update(object, options) {
+  return new Promise(function (fulfill, reject) {
+    //returns an array containing the number of affected rows
+    fulfill([1]);
+  })
+}
+
+function findAll(options) {
+  return new Promise(function (fulfill, reject) {
+    var object = {};
+    if (options && options.where) {
+      var where = options.where;
+
+      for (var key in where) {
+        object[key] = where[key];
+      }
+    }
+
+    //returns an array of objects
+    fulfill([object]);
+  });
+}
+
+function findOne(options) {
+  return new Promise(function (fulfill, reject) {
+    var object = {};
+    if (options && options.where) {
+      var where = options.where;
+
+      for (var key in where) {
+        object[key] = where[key];
+      }
+    }
+
+    //returns object
+    fulfill(object);
+  });
+}
+
+function findOne_user(object) {
     return new Promise(function (fulfill, reject) {
       object.id = 100;
       object.email = "test123@sample.com";
@@ -24,4 +73,12 @@ function findOne(object) {
       object.password = "1c8e432462648d825ade4983da4b1c9cc231180d3dd0e77b0cfe0b28c5e2f2b39aa3adabfcd5e1fe968b9e815005cf67499c30177f4c0199e39064ceaa5adefa";
       fulfill(object);
     });
+}
+
+
+function destroy(options) {
+  return new Promise(function (fulfill, reject) {
+    //returns the number of rows affected
+    fulfill(1);
+  });
 }
