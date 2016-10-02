32f27167edadee8eb35d499861b128906bca1f60
harley1011 <harley.1011@gmail.com>
2016-10-02 00:57:57 -0400

    Fixing error reporting for login.component

--- src/client/app/login/login.component.ts
+++ src/client/app/login/login.component.ts
@@ -1,30 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { User } from '../shared/models/user';
-import { AuthService }       from '../services/auth.service';
-import { Router } from '@angular/router';
+import {User} from '../shared/models/user';
+import {AuthService}       from '../services/auth.service';
+import {Router} from '@angular/router';
 
 /**
-*	This class represents the lazy loaded LoginComponent.
-*/
+ *  This class represents the lazy loaded LoginComponent.
+ */
 
 @Component({
-	moduleId: module.id,
-	selector: 'login-cmp',
-	templateUrl: 'login.component.html'
+  moduleId: module.id,
+  selector: 'login-cmp',
+  templateUrl: 'login.component.html'
 })
 
 export class LoginComponent {
-	user = new User('', '');
-	errorMessage: string;
+  user = new User('', '');
+  errorMessage: string;
 
-	constructor (private authService: AuthService,
-				private router: Router,) {}
+  constructor(private authService: AuthService,
+              private router: Router,) {
+  }
 
-	onSubmit() {
-		this.authService.authenticateUser(this.user)
-                        .subscribe(generalResponse => {
-                            this.router.navigate(['/dashboard/home']);
-				        }, error =>  this.errorMessage = <any>error);
-    }
+  onSubmit() {
+    this.authService.authenticateUser(this.user)
+      .subscribe(generalResponse =>
+          this.router.navigate(['/dashboard/home'])
+        , error => this.errorMessage = error);
+  }
 }
