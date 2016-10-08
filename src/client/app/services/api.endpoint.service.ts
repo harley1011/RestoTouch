045421c771a1fb0e588380af06e63f8c11de37e5
harley1011 <harley.10111@gmail.com>
2016-10-08 13:19:05 -0400

    Adding  check to see if api endpoint will point to relative url or local dev for the api based on if the solution is running in prod or dev

--- src/client/app/services/api.endpoint.service.ts
+++ src/client/app/services/api.endpoint.service.ts
@@ -2,7 +2,17 @@ import {Injectable}     from '@angular/core';
 
 @Injectable()
 export class ApiEndpointService {
-   public apiEndpoint = 'http://localhost:10010';
+  public apiEndpoint = 'http://localhost:10010';
+  private env = '<%= ENV %>';
+
+  constructor() {
+    console.log(this.env);
+    if (this.env === 'dev') {
+      this.apiEndpoint = 'http://localhost:10010';
+    } else {
+      this.apiEndpoint = '';
+    }
+  }
 
   getEndpoint() {
     return this.apiEndpoint;
