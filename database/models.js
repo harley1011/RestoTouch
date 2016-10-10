c06814826fe4461f107be88647fb98005d407e72
Tamy <thuynhse@outlook.com>
2016-10-10 15:22:53 -0400

    added categoryModel into models.js #5

--- database/models.js
+++ database/models.js
@@ -3,11 +3,13 @@ var configDB = require('../config/database.js');
 var sequelize = new Sequelize(configDB.url);
 var userModel = sequelize.import('./models/users.js');
 var restaurantModel = sequelize.import('./models/restaurants.js');
+var categoryModel = sequelize.import('./models/categories.js');
 
 restaurantModel.belongsTo(userModel, {onDelete: 'cascade', foreignKey: 'userId'});
 
 userModel.sync();
 restaurantModel.sync();
+categoryModel.sync();
 
 exports.getUserModel = function() {
   return userModel;
@@ -15,4 +17,8 @@ exports.getUserModel = function() {
 
 exports.getRestaurantModel = function() {
   return restaurantModel;
+};
+
+exports.getCategoryModel = function() {
+  return categoryModel;
 };
