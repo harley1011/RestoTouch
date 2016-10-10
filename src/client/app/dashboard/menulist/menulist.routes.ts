b8c2cadba8afc0ff58c9e6e2b27ad2901f5c2881
David Bastien <david.bastien5@gmail.com>
2016-10-10 13:23:17 -0400

    Created files for menu list page #2

---
+++ src/client/app/dashboard/menulist/menulist.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { MenuListComponent } from './index';
+
+export const MenuListRoutes: Route[] = [
+  	{
+    	path: 'menulist',
+    	component: MenuListComponent
+  	}
+];
