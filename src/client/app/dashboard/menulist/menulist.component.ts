66035e826f2194de0b0f44615d8ff60aa9f94aad
David Bastien <david.bastien5@gmail.com>
2016-10-10 14:03:28 -0400

    Done menu API #2

--- src/client/app/dashboard/menulist/menulist.component.ts
+++ src/client/app/dashboard/menulist/menulist.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { Menu } from '../menu/menu';
+
 @Component({
 	moduleId: module.id,
 	selector: 'menulist-cmp',
@@ -9,14 +11,29 @@ import { Router } from '@angular/router';
 })
 
 export class MenuListComponent implements OnInit {
+	numOfMenus: number;
+	menus: Menu[];
 
   constructor(private router: Router) { }
 
 	getMenus(): void {
-		console.log('Get Menus');
+		this.menus = [
+			new Menu('Menu 1'),
+			new Menu('Menu 2'),
+			new Menu('Menu 3')
+		];
+		this.numOfMenus = this.menus.length;
 	};
 
   ngOnInit(): void {
     this.getMenus();
   }
+
+	add(): void {
+		this.router.navigate(['/dashboard/menu']);
+	}
+
+	modify(menu: Menu): void {
+		this.router.navigate(['/dashboard/menu', menu.name]);
+	}
 }
