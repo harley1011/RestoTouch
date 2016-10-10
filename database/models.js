fa2cac6d9a4c024381bbd59d920beecc9ebdc7a0
David Bastien <david.bastien5@gmail.com>
2016-10-10 17:22:12 -0400

    Working on Restaurant Menu List and the RestaurantMenu relation #2

--- database/models.js
+++ database/models.js
@@ -5,14 +5,19 @@ var userModel = sequelize.import('./models/users.js');
 var restaurantModel = sequelize.import('./models/restaurants.js');
 var categoryModel = sequelize.import('./models/categories.js');
 var menuModel = sequelize.import('./models/menus.js');
+var restaurantMenuModel = sequelize.import('./models/restaurantmenu.js');
 
 restaurantModel.belongsTo(userModel, {onDelete: 'cascade', foreignKey: 'userId'});
 menuModel.belongsTo(userModel, {onDelete: 'cascade', foreignKey: 'userId'});
+
+restaurantModel.belongsToMany(menuModel, {through: restaurantMenuModel});
+menuModel.belongsToMany(restaurantModel, {through: restaurantMenuModel});
 
 userModel.sync();
 restaurantModel.sync();
 categoryModel.sync();
 menuModel.sync();
+restaurantMenuModel.sync();
 
 exports.getUserModel = function() {
   return userModel;
