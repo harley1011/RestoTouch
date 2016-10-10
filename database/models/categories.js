c8215662a4ff645ee7a95a5f566109083a2a7fe3
Tamy <thuynhse@outlook.com>
2016-10-10 17:53:20 -0400

    updated databse to include foreign key restaurant - #5

--- database/models/categories.js
+++ database/models/categories.js
@@ -1,6 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define("Categories", {
-    catId: {type: DataTypes.INTEGER, unique: true},
+    catId: { type: DataTypes.INTEGER, primaryKey: true},
     categoryName: DataTypes.STRING,
+    restaurantId: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This is a reference to another model
+        model: Restaurants,
+        // This is the column name of the referenced model
+        key: 'id',
+        // This declares when to check the foreign key constraint. PostgreSQL only.
+        deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    }
   });
 };
