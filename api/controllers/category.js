a5c85f7b33e2ff3df066ba10be50d3ecfe1c19f6
Hilary Chan <hilarychan079@gmail.com>
2016-10-15 13:29:26 -0400

    added swagger restful api doc #5

---
+++ api/controllers/category.js
@@ -0,0 +1,40 @@
+var models = require("../../database/models");
+var categoryModel;
+
+setDatabase(models);
+
+module.exports = {
+  getAllCategories: getAllCategories,
+  getCategory: getCategory,
+  addCategory: addCategory,
+  updateCategory: updateCategory,
+  deleteCategory: deleteCategory,
+  setDatabase: setDatabase
+};
+
+function setDatabase (m) {
+  models = m;
+  categoryModel = models.getCategoryModel();
+}
+
+// GET /category
+function getAllCategories(req, res) {
+}
+
+// GET /category/{name}
+function getCategory(req, res) {
+}
+
+// POST /category/{name} 
+function addCategory(req, res) {
+}
+
+// PUT /category/{name} 
+function updateCategory(req, res) {
+}
+
+// DELETE /category/{name}
+function deleteCategory(req, res) {
+}
+
+
