64f18a7a08f3b601b6ffe295d0751c9e0900edf8
Tamy <thuynhse@outlook.com>
2016-10-14 23:26:13 -0400

    working on progress. db #5

--- database/models/categories.js
+++ database/models/categories.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define("Categories", {
-    catId: { type: DataTypes.INTEGER, primaryKey: true},
+    catId: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
     categoryName: DataTypes.STRING,
     restaurantId: {
       type: DataTypes.INTEGER,
