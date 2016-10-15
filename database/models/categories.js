2b5e882cf1158a5cb1ce9635eda52cea5bd32ecb
Tamy <thuynhse@outlook.com>
2016-10-15 09:59:29 -0400

    remove foreign key db #5

--- database/models/categories.js
+++ database/models/categories.js
@@ -2,16 +2,6 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define("Categories", {
     catId: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
     categoryName: DataTypes.STRING,
-    restaurantId: {
-      type: DataTypes.INTEGER,
-      references: {
-        // This is a reference to another model
-        model: Restaurants,
-        // This is the column name of the referenced model
-        key: 'id',
-        // This declares when to check the foreign key constraint. PostgreSQL only.
-        deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-      }
     }
   });
 };
