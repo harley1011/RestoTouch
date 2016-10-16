be6e12b3e548cf2368d79f4ddfd43e5e61141317
harley1011 <harley.10111@gmail.com>
2016-10-16 13:28:19 -0400

    Garbage day :D, removing all the default components that came with the template that we aren't using. See README for info on how to access these.

--- src/client/app/dashboard/bs-element/bsElement.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var index_1 = require('./index');
-exports.BSElementRoutes = [
-    {
-        path: 'element',
-        component: index_1.BSElementComponent
-    }
-];
-//# sourceMappingURL=bsElement.routes.js.map
--- src/client/app/dashboard/dashboard.routes.ts
+++ src/client/app/dashboard/dashboard.routes.ts
@@ -5,13 +5,6 @@ import { RestaurantRoutes } from './restaurant/index';
 import { CategoryRoutes } from './category/index';
 import { MenuListRoutes } from './menulist/index';
 import { MenuRoutes } from './menu/index';
-import { ChartRoutes } from './charts/index';
-import { BlankPageRoutes } from './blank-page/index';
-import { TableRoutes } from './tables/index';
-import { FormRoutes } from './forms/index';
-import { GridRoutes } from './grid/index';
-import { BSComponentRoutes } from './bs-component/index';
-import { BSElementRoutes } from './bs-element/index';
 
 import { DashboardComponent } from './index';
 import { AuthService} from '../services/auth.service';
@@ -25,14 +18,7 @@ export const DashboardRoutes: Route[] = [
         ...CategoryRoutes,
         ...RestaurantRoutes,
         ...MenuListRoutes,
-        ...MenuRoutes,
-	    	...ChartRoutes,
-	    	...BSComponentRoutes,
-        ...TableRoutes,
-	    	...BlankPageRoutes,
-        ...FormRoutes,
-        ...GridRoutes,
-        ...BSElementRoutes
+        ...MenuRoutes
     	],
       canActivate: [AuthService],
       canActivateChild: [AuthService]
