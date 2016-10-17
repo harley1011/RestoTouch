4cef2de994931c1b6d0e780452d65e8d5cd8dbfd
harley1011 <harley.1011@gmail.com>
2016-10-17 12:26:48 -0400

    Updating production files

--- dist/prod/app/dashboard/charts/chart.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var index_1 = require('./index');
-exports.ChartRoutes = [
-    {
-        path: 'chart',
-        component: index_1.ChartComponent
-    }
-];
-//# sourceMappingURL=chart.route.js.map
