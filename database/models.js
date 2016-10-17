19980bff61217c1da32e51f2cb76d1f79e0cb9e0
harley1011 <harley.1011@gmail.com>
2016-10-16 21:04:13 -0400

    Improving populate script to use menus array

--- database/models.js
+++ database/models.js
@@ -12,7 +12,7 @@ var restaurantsTranslationsModel = sequelize.import('./models/restaurantsTransla
 
 // Enable this if you want to drop all tables and create them,
 // DO NOT COMMIT THIS AS TRUE THOUGH
-var dropTable = true;
+var dropTable = false;
 
 userModel.sync({force: dropTable}).then(function () {
 
