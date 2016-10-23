4de3e2e45e5756e623f2714d87b0b64a416b801f
harley1011 <harley.1011@gmail.com>
2016-10-23 17:48:24 -0400

    Adding item model and html for item list #3

--- src/client/app/dashboard/item-list/item-list.component.ts
+++ src/client/app/dashboard/item-list/item-list.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {Item } from './../../shared/models/items';
 
 @Component({
   moduleId: module.id,
@@ -7,6 +7,14 @@ import {Component} from '@angular/core';
   templateUrl: 'item-list.component.html',
 })
 
-export class ItemListComponent {
+export class ItemListComponent implements OnInit {
+  items: Array<Item>;
 
+  ngOnInit(): void {
+    this.items = [new Item('Hot dog', 'A yummy hot dog')];
+  }
+
+  add(): void {
+    this.items.push(new Item('Drink', 'A drinl'));
+  }
 }
---
+++ src/client/app/shared/models/items.ts
@@ -0,0 +1,7 @@
+export class Item {
+  id: number;
+  constructor (
+    public name: string,
+    public description: string
+  ) { }
+}
