6d2788e493c6b95e2152cadea98f509a36e67c36
harley1011 <harley.1011@gmail.com>
2016-10-23 13:55:07 -0400

    Adding item-list components #3

---
+++ src/client/app/dashboard/items/item-list.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'restaurant-cmp',
+  templateUrl: 'restaurant.component.html',
+})
+
+export class ItemListComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/client/app/dashboard/items/item-list.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { ItemListComponent } from '.';
+
+export const ItemListRoutes: Route[] = [
+  {
+  	path: 'item',
+  	component: ItemListComponent
+  }
+];
