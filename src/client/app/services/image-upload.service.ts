5509efa9dfea4ffac9e06f76f44b01e14d27909c
harley1011 <harley.1011@gmail.com>
2016-11-10 21:58:48 -0500

    Using raw xhrrequest so that we can use onprogress #52

--- src/client/app/services/image-upload.service.ts
+++ src/client/app/services/image-upload.service.ts
@@ -1,29 +1,53 @@
 import {Injectable}     from '@angular/core';
 import {AuthHttpService} from './auth-http.services';
 import {ApiEndpointService} from './api-endpoint.service';
-import { Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-
+import {Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {Http} from '@angular/http';
 @Injectable()
 export class ImageUploadService {
-  private url = '/sign-s3';
-
-  constructor(private http: AuthHttpService, private api: ApiEndpointService ) {
+  constructor(private http: AuthHttpService, private rawHttp: Http, private api: ApiEndpointService) {
   }
 
   getS3Key(imageName: string, imageType: string) {
     return this.http
-      .get(this.api.getEndpoint() + this.url + '?imageName=' + imageName + '&imageType=' + imageType)
+      .get(this.api.getEndpoint() + '/s3ImageUploadUrl' + '?imageName=' + imageName + '&imageType=' + imageType)
       .map(this.extractData)
       .catch(this.handleError);
   }
 
-  private extractData(res: Response) {
-    let body = res.json();
-    return body || { };
+  uploadImage(url: string, signedRequest: string,  file: any) {
+    // this.rawHttp.put(signedRequest, file).subscribe(response => {
+    //   console.log(response);
+    //   return url;
+    // }, error => {
+    //   console.log(error);
+    // });
+    const xhr = new XMLHttpRequest();
+    xhr.open('PUT', signedRequest);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log('works');
+        } else {
+          console.log('Could not upload file.');
+        }
+      }
+    };
+
+    xhr.upload.onprogress = (event) => {
+      console.log(Math.round(event.loaded / event.total * 100));
+    };
+    xhr.send(file);
+    return url;
   }
 
-  private handleError (error: any) {
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || {};
+  }
+
+  private handleError(error: any) {
     // In a real world app, we might use a remote logging infrastructure
     // We'd also dig deeper into the error to get a better message
     let errMsg = (error.message) ? error.message :
