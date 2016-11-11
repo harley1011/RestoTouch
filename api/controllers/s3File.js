344e50583f1799e6089e44b2d2b5816320feb43a
harley1011 <harley.1011@gmail.com>
2016-11-10 21:03:39 -0500

    Adding swagger definition file for s3File image upload #52

--- api/controllers/s3File.js
+++ api/controllers/s3File.js
@@ -1,17 +1,42 @@
-var models = require("../../database/models");
-var itemModel;
-var itemSizeModel;
+var aws = require('aws-sdk');
+var S3_BUCKET = 'resto-item-images';
+var uuid = require('node-uuid');
 
-var _ = require('lodash');
+aws.config.update({
+  accessKeyId: 'AKIAIEQO625YFT5PBZ3A',
+  secretAccessKey: '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',
+  region: 'us-east-1'
+});
 
-
-setDatabase(models);
 
 module.exports = {
-  signS3UploadImageKey: signS3UploadImageKey
+  getS3UploadImageKey: getS3UploadImageKey
 };
 
 
-function signS3UploadImageKey() {
+function getS3UploadImageKey() {
+  var s3 = new aws.S3();
+  var fileName = req.query['imageName'];
+  var fileType = req.query['imageType'];
 
+  var s3Params = {
+    Bucket: S3_BUCKET,
+    Key: uuid.v4(),
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, function (err, data) {
+    if (err) {
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+        signedRequest: data,
+        url: 'https://' + S3_BUCKET + '.s3.amazonaws.com/' + fileName
+      };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
 }
