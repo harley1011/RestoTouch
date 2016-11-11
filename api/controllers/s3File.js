a0944da6795f810652300dbac54ecc838f298059
harley1011 <harley.1011@gmail.com>
2016-11-10 20:20:39 -0500

    Adding s3File controller for interacting with s3 server #52

---
+++ api/controllers/s3File.js
@@ -0,0 +1,17 @@
+var models = require("../../database/models");
+var itemModel;
+var itemSizeModel;
+
+var _ = require('lodash');
+
+
+setDatabase(models);
+
+module.exports = {
+  signS3UploadImageKey: signS3UploadImageKey
+};
+
+
+function signS3UploadImageKey() {
+
+}
