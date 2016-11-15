75c89e234e697bd6ae181945d17e9b20fb2692f5
Samer El-Achkar <samerelachkarmail@gmail.com>
2016-11-15 18:04:43 -0500

    Fixing imports that are no longer necessary

--- src/client/app/dashboard/home/home.module.ts
+++ src/client/app/dashboard/home/home.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { CarouselModule, DropdownModule, AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+
 
 @NgModule({
-    imports: [CommonModule, CarouselModule, DropdownModule, AlertModule],
+    imports: [CommonModule],
     declarations: [HomeComponent],
     exports: [HomeComponent]
 })
--- src/client/app/dashboard/item-list/item-list.module.ts
+++ src/client/app/dashboard/item-list/item-list.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ItemListComponent } from './item-list.component';
-import { CarouselModule, DropdownModule, AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+
 
 @NgModule({
-    imports: [CommonModule, CarouselModule, DropdownModule, AlertModule, FormsModule],
+    imports: [CommonModule, FormsModule],
     declarations: [ItemListComponent],
     exports: [ItemListComponent]
 })
