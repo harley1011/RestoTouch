f0c30bc74750de443788532c244133755fd9d9d0
Tex08 <alex@pelletier.com>
2016-11-20 23:36:47 -0500

    Modified put menu api to include translations #40

--- api/controllers/menu.js
+++ api/controllers/menu.js
@@ -82,6 +82,9 @@ function updateMenu(req, res) {
     include: [{
       model: menuLanguageModel,
       as: 'supportedLanguages'
+    }, {
+      model: menuTranslationsModel,
+      as: 'translations'
     }]
   }).then(function (oldMenu) {
 
@@ -89,12 +92,30 @@ function updateMenu(req, res) {
     var languagesToAdd = _.differenceBy(menu.supportedLanguages, oldMenu.supportedLanguages, 'languageCode');
 
     for (var prop in menu) {
-      oldMenu[prop] = menu[prop];
+      if(prop != 'translations')
+          oldMenu[prop] = menu[prop];
     }
+
+    oldMenu.translations.forEach(function(translation) {
+      var newTranslation = _.find(menu.translations, function (tr) {return tr.languageCode === translation.languageCode});
+      for (var prop in newTranslation) {
+          translation[prop] = newTranslation[prop];
+      }
+      translation.save();
+      _.remove(menu.translations, function (tr) {return tr.languageCode === translation.languageCode});
+    });
+
+    menu.translations.forEach(function (translation) {
+      translation.menuId = menu.id;
+    });
+    // Create the new translations
+    menuTranslationsModel.bulkCreate(menu.translations);
 
     oldMenu.save().then(function (result) {
       languagesToRemove.forEach(function (language) {
         menuLanguageModel.destroy({where: {'languageCode': language.languageCode, 'menuId': menu.id}});
+        menuTranslationsModel.destroy({where: {'languageCode': language.languageCode, 'menuId': menu.id}});
+        _.remove(oldMenu.translations, function (translation) { return translation.languageCode == language.languageCode});
       })
 
       languagesToAdd.forEach(function (language) {
