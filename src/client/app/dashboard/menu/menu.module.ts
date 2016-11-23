88f5becfb153c4e16c4a1c744c7860f89ae3a300
harley1011 <harley.1011@gmail.com>
2016-11-22 19:45:29 -0500

    Moving divs into formContainer div so that they render

--- src/client/app/dashboard/menu/menu.module.ts
+++ src/client/app/dashboard/menu/menu.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MenuComponent} from './menu.component';
-import {DragulaModule} from '../../../../../node_modules/ng2-dragula/ng2-dragula';
+import {DragulaModule} from 'ng2-dragula/ng2-dragula';
 
 @NgModule({
   imports: [CommonModule, DragulaModule],
