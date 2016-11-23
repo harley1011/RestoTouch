1e2ccc69b8ec910605f3eaf128ae04812cb6e88b
Samer El-Achkar <samerelachkarmail@gmail.com>
2016-11-23 16:55:10 -0500

    Cleaning ng2-dragula from menu module #39

--- src/client/app/dashboard/menu/menu.module.ts
+++ src/client/app/dashboard/menu/menu.module.ts
@@ -1,10 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MenuComponent} from './menu.component';
-import {DragulaModule} from 'ng2-dragula/ng2-dragula';
 
 @NgModule({
-  imports: [CommonModule, DragulaModule],
+  imports: [CommonModule],
   declarations: [MenuComponent],
   exports: [MenuComponent],
 })
