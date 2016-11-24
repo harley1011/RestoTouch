550b4f1d9201d0ff81b35dbbcae615c932dcdfe7
Samer El-Achkar <samerelachkarmail@gmail.com>
2016-11-24 16:02:36 -0500

    Frontend test for menu-category #39

--- src/client/app/dashboard/menu/menu-category.spec.ts
+++ src/client/app/dashboard/menu/menu-category.spec.ts
@@ -5,6 +5,9 @@ import {AuthHttpService} from '../../services/auth-http.services';
 import {ApiEndpointService} from '../../services/api-endpoint.service';
 import {Router} from '@angular/router';
 import {MenuCategoryService} from './menu-catergory.service';
+import {Menu} from '../../shared/models/menu';
+import {Category} from '../../shared/models/category';
+
 
 export function main() {
 
@@ -48,15 +51,74 @@ export function main() {
 
     it('should add category to Menu', () => {
 
+      var mockCategories = Category[new Category('Salads'), new Category('Burgers'),new Category('Sweets')];
+
+      var mockMenu = new Menu('Lunch Menu', mockCategories);
+      var mockSandwishes = new Category('Sandwishes');
+
+        initialResponse = menuCategoryService.addMenuCategory(mockMenu.id, mockSandwishes.id);
+
+        connection.mockRespond(new Response(new ResponseOptions({body: '{"success": 1, "description": "Menu Category Added"}'})));
+
+        let response: any;
+        initialResponse.subscribe(
+          (res: any) => {
+            response = res;
+          }
+        );
+
+        expect(response).toEqual({
+          success: 1,
+          description: 'Menu Category Added'
+        });
 
     });
 
     it('should update category to Menu', () => {
+      var mockCategories = [new Category('Salads'), new Category('Burgers') , new Category('Sweets')];
 
+      var mockMenu1= new Menu('Lunch Menu', mockCategories);
+      var mockMenu2 = new Menu('Dinner Menu', []);
+
+      for (let i=0; i < mockCategories.length; i++) {
+        initialResponse = menuCategoryService.updateMenuCategory(mockMenu2.id, mockMenu1.categories[i].id);
+
+        connection.mockRespond(new Response(new ResponseOptions({body: '{"success": 1, "description": "Menu Category Updated"}'})));
+
+        let response: any;
+        initialResponse.subscribe(
+          (res: any) => {
+            response = res;
+          }
+        );
+
+        expect(response).toEqual({
+          success: 1,
+          description: 'Menu Category Updated'
+        });
+      }
     });
 
     it('should delete category to Menu', () => {
 
+      var mockCategories = [new Category('Salads'), new Category('Burgers'), new Category('Sweets')];
+
+      var mockMenu = new Menu('Lunch Menu', mockCategories);
+
+      initialResponse = menuCategoryService.deleteMenuCategory(mockMenu.id, mockMenu.categories[0].id);
+      connection.mockRespond(new Response(new ResponseOptions({body: '{"success": 1, "description": "Menu Category Deleted"}'})));
+
+      let response: any;
+      initialResponse.subscribe(
+        (res: any) => {
+          response = res;
+        }
+      );
+
+      expect(response).toEqual({
+        success: 1,
+        description: 'Menu Category Deleted'
+      });
     });
 
   });
--- src/client/app/shared/models/menu.ts
+++ src/client/app/shared/models/menu.ts
@@ -1,9 +1,9 @@
 import {Category} from './category';
 
 export class Menu {
-  id: number;
   constructor (
     public name: string,
-    public categories: Category [] // This menu will contain the list of selected categories.
+    public categories: Category [], // This menu will contain the list of selected categories.
+    public id?: number
 	) { }
 }
