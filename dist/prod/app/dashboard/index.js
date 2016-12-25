7f43c0589eab296fc4d4a26d87dd013d4515784c
harley1011 <harley.1011@gmail.com>
2016-12-25 17:31:04 -0500

    Revert "Merge branch 'master' of https://github.com/mgechev/angular2-seed into develop and fixing routing issues"
    This reverts commit c414e0d4e4255e9a4ba9696f3093c9fa480f62a0, reversing
    changes made to fe0dc91c71238c5a10742e9807127cf957614a4d.

---
+++ dist/prod/app/dashboard/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+/**
+*	This barrel file provides the export for the lazy loaded DashboardComponent.
+*/
+__export(require('./dashboard.component'));
+__export(require('./dashboard.routes'));
