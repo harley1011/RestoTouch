b3f582d8d6f6e1a8a1552064d9ae915f4e5dc533
Tamy <thuynhse@outlook.com>
2016-12-25 17:59:29 -0500

    #14, #5 incorporated language in html, completed

--- src/client/app/dashboard/category/category.module.ts
+++ src/client/app/dashboard/category/category.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './category.component';
 import { CarouselModule, DropdownModule, AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import {TranslateModule} from 'ng2-translate';
 
 @NgModule({
-    imports: [CommonModule, CarouselModule, DropdownModule, AlertModule],
+    imports: [CommonModule, CarouselModule, DropdownModule, AlertModule, TranslateModule],
     declarations: [CategoryComponent],
     exports: [CategoryComponent]
 })
