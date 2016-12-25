c59f2a7baeec5559def455954bc7076a66eec9e8
Tamy <thuynhse@outlook.com>
2016-12-25 09:03:20 -0500

    #14, #55 incorporated language to main of dashboard

--- src/client/app/dashboard/home/home.component.ts
+++ src/client/app/dashboard/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HomeService } from './home.service';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../shared/models/user';
+import {TranslateService} from 'ng2-translate';
 
 //import { ActivatedRoute } from '@angular/router';
 
@@ -15,8 +16,13 @@ import { User } from '../../shared/models/user';
 export class HomeComponent implements OnInit {
     user: User;
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService,
+                private translate: TranslateService) {
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang('en');
+                }
+
     ngOnInit() {
-            this.user = this.authService.loggedInUser;
+        this.user = this.authService.loggedInUser;
     }
 }
