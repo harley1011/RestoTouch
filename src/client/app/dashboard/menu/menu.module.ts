3db3c519d2e315d3be3275a971e3b5ec967053b5
Tamy <thuynhse@outlook.com>
2016-12-25 17:42:44 -0500

    #14, #2 incorporated language in html, completed

--- src/client/app/dashboard/menu/menu.module.ts
+++ src/client/app/dashboard/menu/menu.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu.component';
 import { FormsModule } from '@angular/forms';
 import { CarouselModule, DropdownModule, AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import {TranslateModule} from 'ng2-translate';
 
 @NgModule({
-    imports: [CommonModule, CarouselModule, DropdownModule, AlertModule, FormsModule],
+    imports: [CommonModule, CarouselModule, DropdownModule, AlertModule, FormsModule, TranslateModule],
     declarations: [MenuComponent],
     exports: [MenuComponent]
 })
