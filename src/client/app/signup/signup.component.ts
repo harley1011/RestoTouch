820c5d86cd388c5f7fd8f997206e66e721b97d9d
Tamy <thuynhse@outlook.com>
2016-12-24 21:20:00 -0500

    #14, #6 added translation for error msg

--- src/client/app/signup/signup.component.ts
+++ src/client/app/signup/signup.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { User } from '../shared/models/user';
 import { AuthService }       from '../services/auth.service';
 import { Router } from '@angular/router';
+import {TranslateService} from 'ng2-translate';
 
 /**
 *	This class represents the lazy loaded SignupComponent.
@@ -17,13 +18,23 @@ import { Router } from '@angular/router';
 export class SignupComponent {
   user = new User('', '', '', '', '');
   errorMessage = '';
+  currentLanguage ='';
 
   constructor (private authService: AuthService,
-               private router: Router,) {}
+               private router: Router,
+               private translate: TranslateService,) {
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang('en');
+        this.currentLanguage = translate.currentLang;
+        }
 
   onSubmit() {
      if (this.user.password !== this.user.passwordConfirm) {
+       if(this.currentLanguage === 'en') {
        this.errorMessage = 'Passwords do not match';
+       } else {
+         this.errorMessage = 'Les mots de passe ne se concordent pas';
+       }
        return;
      }
      this.authService.registerUser(this.user)
