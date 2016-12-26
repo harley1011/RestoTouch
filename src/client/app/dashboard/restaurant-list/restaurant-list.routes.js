ce14f0c03e1bcfd7ee3f93fb4d0b041a0c00bf49
harley1011 <harley.1011@gmail.com>
2016-12-25 21:21:41 -0500

    Removing js files and adding them to gitignore

--- src/client/app/dashboard/restaurant-list/restaurant-list.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var restaurant_list_component_1 = require('./restaurant-list.component');
-exports.RestaurantsRoutes = [
-    {
-        path: 'restaurants',
-        component: restaurant_list_component_1.RestaurantListComponent
-    }
-];
--- src/client/app/shared/config/env.config.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-exports.Config = JSON.parse('<%= ENV_CONFIG %>');
