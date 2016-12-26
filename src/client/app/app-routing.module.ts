9805f2947b7b3b2735984df50893a802e8f37b86
David Bastien <david.bastien5@gmail.com>
2016-12-26 11:30:56 -0500

    Fixing routing for signup

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: '', component: LoginComponent }
+      { path: '', component: LoginComponent },
+      { path: 'signup', component: SignupComponent }
       /* define app module routes here, e.g., to lazily load a module
        (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
        */
@@ -15,4 +17,3 @@ import {LoginComponent} from './login/login.component';
 })
 
 export class AppRoutingModule { }
-
