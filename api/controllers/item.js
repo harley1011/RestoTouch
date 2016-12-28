f19218a689123ff546e77bce8b9e48252a18c906
harley1011 <harley.1011@gmail.com>
2016-12-28 18:07:06 -0500

    Adding update for ingredients and ingredients groups in item update #4

--- api/controllers/item.js
+++ api/controllers/item.js
@@ -36,7 +36,7 @@ function getAll(req, res) {
   return itemModel.findAll({
     where: {userId: req.userId},
     include: [{model: itemSizeModel, as: 'sizes'},
-              {model: itemTranslationModel, as: 'translations'}],
+      {model: itemTranslationModel, as: 'translations'}],
     //order: ['name']
   }).then(function (items) {
     return res.json({items: items});
@@ -114,44 +114,85 @@ function update(req, res) {
   var item = req.body;
 
   itemModel.findOne({
-    where: {id: item.id},
-    include: [{model: itemSizeModel, as: 'sizes'},
-              {model: itemLanguageModel, as: 'supportedLanguages'},
-              {model: itemTranslationModel, as: 'translations'}]
-  }).then(function (oldItem) {
+      where: {id: item.id},
+      include: [{model: itemSizeModel, as: 'sizes'},
+        {model: itemLanguageModel, as: 'supportedLanguages'},
+        {model: itemTranslationModel, as: 'translations'},
+        {
+          model: ingredientGroupModel, as: 'ingredientGroups',
+          include: [{
+            model: ingredientModel,
+            as: 'ingredients'
+          }]
+        }]
+    }
+  ).then(function (oldItem) {
 
     var sizesToRemove = _.differenceBy(oldItem.sizes, item.sizes, 'id');
     var sizesToAdd = _.differenceBy(item.sizes, oldItem.sizes, 'id');
     var languagesToRemove = _.differenceBy(oldItem.supportedLanguages, item.supportedLanguages, 'languageCode');
     var languagesToAdd = _.differenceBy(item.supportedLanguages, oldItem.supportedLanguages, 'languageCode');
 
-    if (item.imageUrl != oldItem.imageUrl)
-    {
+    var ingredientGroupsToAdd = _.differenceBy(item.ingredientGroups, oldItem.ingredientGroups, 'name');
+    var ingredientGroupsToRemove = _.differenceBy(oldItem.ingredientGroups, item.ingredientGroups, 'name');
+    var ingredientGroupsToCheckForUpdate = _.differenceBy(_.differenceBy(item.ingredientGroups, ingredientGroupsToRemove, 'name'), ingredientGroupsToRemove, 'name');
+
+    if (item.imageUrl != oldItem.imageUrl) {
       var split = oldItem.imageUrl.split('/');
       req.imageKey = split[split.length - 1];
-      s3File.deleteImage(req,res);
+      s3File.deleteImage(req, res);
     }
 
     for (var prop in item) {
-      if(prop != 'translations')
+      if (prop != 'translations')
         oldItem[prop] = item[prop];
     }
+
+    ingredientGroupsToAdd.forEach(function (ingredientGroup) {
+      ingredientGroup.itemId = oldItem.id;
+    });
+
+    ingredientGroupModel.bulkCreate(ingredientGroupsToAdd);
+
     sizesToAdd.forEach(function (size) {
       size.itemId = oldItem.id;
-    })
+    });
+
+    ingredientGroupsToCheckForUpdate.forEach(function (ingredientGroup) {
+      var ingredientGroupToCheck = _.find(oldItem.ingredientGroups, {'id': ingredientGroup.id});
+      var ingredientToAdd = _.differenceBy(ingredientGroup.ingredients, ingredientGroupToCheck.ingredients, 'id');
+      var ingredientToRemove = _.differenceBy(ingredientGroupToCheck.ingredients, ingredientGroup.ingredients, 'id');
+      ingredientToAdd.forEach(function (ingredient) {
+        ingredient.ingredientGroupId = ingredientGroup.id;
+      });
+      ingredientModel.bulkCreate(ingredientToAdd);
+
+      ingredientToRemove.forEach(function (ingredient) {
+        ingredientModel.destroy({where: {id: ingredient.id}})
+      });
+    });
+
     itemSizeModel.bulkCreate(sizesToAdd);
+
+    ingredientGroupsToRemove.forEach(function (ingredientGroup) {
+      ingredientGroupModel.destroy({where: {id: ingredientGroup.id}})
+    });
 
     sizesToRemove.forEach(function (size) {
       itemSizeModel.destroy({where: {id: size.id}})
-    })
+    });
 
-    oldItem.translations.forEach(function(translation) {
-      var newTranslation = _.find(item.translations, function (tr) {return tr.languageCode === translation.languageCode});
-      for (var prop in newTranslation ) {
+    oldItem.translations.forEach(function (translation) {
+      var newTranslation = _.find(item.translations, function (tr) {
+        return tr.languageCode === translation.languageCode
+      });
+      for (var prop in newTranslation) {
         translation[prop] = newTranslation[prop];
       }
       translation.save();
-      _.remove(item.translations, function (tr) {return tr.languageCode === translation.languageCode});
+      _.remove(item.translations, function (tr) {
+        return tr.languageCode === translation.languageCode
+      });
     });
 
     item.translations.forEach(function (translation) {
@@ -164,7 +205,9 @@ function update(req, res) {
       languagesToRemove.forEach(function (language) {
         itemLanguageModel.destroy({where: {'languageCode': language.languageCode, 'itemId': item.id}});
         itemTranslationModel.destroy({where: {'languageCode': language.languageCode, 'itemId': item.id}});
-        _.remove(oldItem.translations, function (translation) {return translation.languageCode == language.languageCode})
+        _.remove(oldItem.translations, function (translation) {
+          return translation.languageCode == language.languageCode
+        })
       })
 
       languagesToAdd.forEach(function (language) {
@@ -192,7 +235,7 @@ function del(req, res) {
       if (item.imageUrl) {
         var split = item.imageUrl.split('/');
         req.imageKey = split[split.length - 1];
-        s3File.deleteImage(req,res);
+        s3File.deleteImage(req, res);
       }
       item.destroy();
       return res.json({success: 1, description: "Item Deleted"});
