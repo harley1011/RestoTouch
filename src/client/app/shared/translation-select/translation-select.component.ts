5ffd84ef9f53744e50bd9bd5072ef678e8b1ef2a
harley1011 <harley.1011@gmail.com>
2017-01-02 11:28:35 -0500

    Adding translation-select component #80

---
+++ src/client/app/shared/translation-select/translation-select.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+// import {AuthService} from '../../services/auth.service';
+// import {User} from '../models/user';
+// import {LanguageService} from '../../services/language.service';
+// import {Language} from '../../shared/models/language';
+// import { Router } from '@angular/router';
+// import {TranslateService} from 'ng2-translate';
+
+@Component({
+  moduleId: module.id,
+  selector: 'translation-select',
+  templateUrl: 'translation-select.component.html'
+})
+
+export class TranslationSelectComponent implements OnInit {
+
+  constructor() {
+    console.log('temp');
+  }
+
+  ngOnInit() {
+    console.log('temp');
+  }
+
+}
