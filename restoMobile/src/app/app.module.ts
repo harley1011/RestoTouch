cadb3aaedddaa995261c5fd9f403a2415103c460
Hilary Chan <hilarychan079@gmail.com>
2017-01-05 17:56:17 -0500

    setting up login component #79

--- restoMobile/src/app/app.module.ts
+++ restoMobile/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { LoginPage } from '../pages/login/login';
 
 @NgModule({
   declarations: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    LoginPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -17,7 +19,8 @@ import { Page2 } from '../pages/page2/page2';
   entryComponents: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    LoginPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
---
+++ restoMobile/src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {User} from '../shared/models/user';
+import {AuthService}       from '../services/auth.service';
+import {Router} from '@angular/router';
+
+@Component({
+  templateUrl: 'login.component.html'
+})
+
+export class LoginComponent {
+
+}
