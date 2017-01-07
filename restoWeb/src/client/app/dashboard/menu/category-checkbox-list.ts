ff58ac55447119421e9140c628567d0f8988b6a4
David Bastien <david.bastien5@gmail.com>
2017-01-07 12:14:12 -0500

    Making progress on front end issues with checkbox mapping #82

---
+++ restoWeb/src/client/app/dashboard/menu/category-checkbox-list.ts
@@ -0,0 +1,16 @@
+import {Item} from '../../shared/models/items';
+import {Category} from '../../shared/models/category';
+
+export class CategoryCheckboxList {
+  constructor (
+    public category: Category,
+    public items: Array<ItemCheckbox>
+  ) { }
+}
+
+export class ItemCheckbox {
+  constructor (
+    public item: Item,
+    public enabled: boolean
+  ) { }
+}
--- restoWeb/src/client/app/dashboard/menu/menu.component.ts
+++ restoWeb/src/client/app/dashboard/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import {MenuService} from './menu.service';
+import {CategoryCheckboxList, ItemCheckbox} from './category-checkbox-list';
 import {Category} from '../../shared/models/category';
 import {CategoryService} from '../category/category.service';
 import {Menu, MenuTranslations} from '../../shared/models/menu';
@@ -8,6 +9,7 @@ import {Item} from '../../shared/models/items';
 import {LanguageService} from '../../services/language.service';
 import {Language} from '../../shared/models/language';
 import {TranslationSelectComponent} from '../../shared/translation-select/translation-select.component';
+import {ItemCategory} from '../../shared/models/item-category';
 
 // This tells angular that MenuComponent class is actually an component which we put metadata on it.
 @Component({
@@ -21,10 +23,12 @@ export class MenuComponent implements OnInit {
   create: boolean;
   errorMessage: string;
   menu: Menu; // Menu has an array of selected categories that represent Category List
-  availableCategories: Array<Category> = [];// This is the Available Category List
+  availableCategories: Array<Category> = []; // This is the Available Category List
+  itemCategories: Array<CategoryCheckboxList> = []; //list of all ItemCategory objects related to the categories in the menu
 
   @ViewChild(TranslationSelectComponent)
   private translationSelectComponent: TranslationSelectComponent;
+
   // We are using dependency injection to get instances of these services into our component.
   constructor(private route: ActivatedRoute,
               private menuService: MenuService,
@@ -41,7 +45,7 @@ export class MenuComponent implements OnInit {
       } else {
         this.getCategories();
         let translation = new MenuTranslations('', this.translationSelectComponent.selectedLanguage.languageCode);
-        this.menu = new Menu([translation], translation, []);
+        this.menu = new Menu([translation], translation, [], []);
         this.create = true;
       }
     });
@@ -53,6 +57,7 @@ export class MenuComponent implements OnInit {
         this.menu = menu;
         this.onSelectLanguage(this.translationSelectComponent.selectedLanguage);
         this.getCategories();
+        this.initializeItemCategories();
       },
       error => {
         this.errorMessage = <any>error;
@@ -75,6 +80,32 @@ export class MenuComponent implements OnInit {
         this.errorMessage = <any>error;
       }
     );
+  }
+
+  initializeItemCategories(): void {
+    let self = this;
+    let catCheckList: CategoryCheckboxList;
+    let itemCheck: ItemCheckbox;
+    let itemCategory: ItemCategory;
+    console.log(this.menu);
+    this.menu.categories.forEach(function (category) {
+      catCheckList = new CategoryCheckboxList(category, []);
+      category.items.forEach(function (item) {
+        itemCheck = new ItemCheckbox(item, true);
+
+        for (var i = 0; i < self.menu.disabledCategoryItems.length; i++) {
+          itemCategory = self.menu.disabledCategoryItems[i]
+          if (itemCategory.categoryId === category.id && itemCategory.itemId === item.id) {
+            itemCheck.enabled = false;
+            break;
+          }
+        }
+
+        catCheckList.items.push(itemCheck);
+      });
+
+      self.itemCategories.push(catCheckList);
+    });
   }
 
   onSelectLanguage(language: Language) {
@@ -112,18 +143,27 @@ export class MenuComponent implements OnInit {
     );
   }
 
-  changeOrder(category: Category, changeIndex: number) {
-     let newIndex = ((this.menu.categories.indexOf(category) - 1 + changeIndex)
+  changeOrder(catCheckList: CategoryCheckboxList, changeIndex: number) {
+    var category = catCheckList.category;
+    let newIndex = ((this.menu.categories.indexOf(category) - 1 + changeIndex)
        % this.menu.categories.length + this.menu.categories.length) % this.menu.categories.length;
-     let currentIndex = this.menu.categories.indexOf(category);
+    let currentIndex = this.menu.categories.indexOf(category);
 
-     this.menu.categories[currentIndex] = this.menu.categories[newIndex];
-     this.menu.categories[newIndex] = category;
+    this.menu.categories[currentIndex] = this.menu.categories[newIndex];
+    this.menu.categories[newIndex] = category;
   }
 
   addCategoryToMenu(category: Category): void {
     this.availableCategories.splice(this.availableCategories.indexOf(category), 1);
     this.menu.categories.push(category);
+
+    var catCheckList = new CategoryCheckboxList(category, []);
+    let itemCheck: ItemCheckbox;
+    category.items.forEach(function (item) {
+      itemCheck = new ItemCheckbox(item, true);
+      catCheckList.items.push(itemCheck);
+    });
+    this.itemCategories.push(catCheckList);
   }
 
   update(): void {
@@ -153,13 +193,15 @@ export class MenuComponent implements OnInit {
     );
   }
 
-  removeCategoryFromMenu(category: Category): void {
+  removeCategoryFromMenu(catCheckList: CategoryCheckboxList): void {
+    var category = catCheckList.category;
     this.menu.categories.splice(this.menu.categories.indexOf(category), 1);
     this.availableCategories.push(category);
+    this.itemCategories.splice(this.itemCategories.indexOf(catCheckList), 1);
   }
 
-  selectItem(item: Item, category: Category): void {
-    console.log('select item ' + item.id + ' and category ' + category.id);
+  selectItem(catCheckList: CategoryCheckboxList, itemCheck: ItemCheckbox): void {
+    console.log('select item ' + itemCheck.item.id + ' and category ' + catCheckList.category.id);
   }
 }
 
