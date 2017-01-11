17e7cad300b6fa3206a1bfb698ab051d1f0e3f95
Samer El-Achkar <samerelachkarmail@gmail.com>
2017-01-11 09:43:51 -0500

    Setup menu component #21

--- restoMobile/src/app/app.module.ts
+++ restoMobile/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { MenuListPage } from '../pages/menu-list/menu-list';
+import {MenuPage} from "../pages/menu/menu";
 
 @NgModule({
   declarations: [
     MyApp,
     Page1,
     Page2,
-    MenuListPage
+    MenuListPage,
+    MenuPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -20,7 +22,8 @@ import { MenuListPage } from '../pages/menu-list/menu-list';
     MyApp,
     Page1,
     Page2,
-    MenuListPage
+    MenuListPage,
+    MenuPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
---
+++ restoMobile/src/pages/menu/menu.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-menu',
+  templateUrl: 'menu.html'
+})
+export class MenuPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+
+}
