30523641339012cdb88da7013c75c10ee4de5504
Hilary Chan <hilarychan079@gmail.com>
2017-01-12 11:06:51 -0500

    login finally works #79

--- restoMobile/src/pages/login/login.ts
+++ restoMobile/src/pages/login/login.ts
@@ -3,7 +3,6 @@ import {Http, Response} from '@angular/http';
 import { NavController, NavParams } from 'ionic-angular';
 import { AuthService } from '../services/auth.service';
 import { User } from '../shared/models/user';
-import {Validators, FormBuilder } from '@angular/forms';
 //import {ApiEndpointService} from '../services/api-endpoint.service';
 import { Page2 } from '../page2/page2';
 
@@ -18,18 +17,13 @@ import { Page2 } from '../page2/page2';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-    user: User;
+    user = new User('', '');
     errorMessage: string;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public authService: AuthService,
-             private formBuilder: FormBuilder) {
+              public authService: AuthService) {
     //this.user = new User('', '', '', '', '');
-      this.user = this.formBuilder.group({
-        email: [''],
-          password: ['']
-      });
     //this.errorMessage = '';
   }
 
