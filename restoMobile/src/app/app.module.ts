a7fa3110fd2f1ae8303e1641714875a994197b4d
Tamy <thuynhse@outlook.com>
2017-01-13 16:32:04 -0500

    #24 set up ng2-translate

--- restoMobile/src/app/app.module.ts
+++ restoMobile/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import {Http} from '@angular/http';
+import { TranslateModule,TranslateStaticLoader,TranslateLoader } from 'ng2-translate/ng2-translate';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { WelcomePage } from '../pages/welcome/welcome';
+
+export function createTranslateLoader(http: Http) {
+    return new TranslateStaticLoader(http, 'assets/languages', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -13,7 +19,12 @@ import { WelcomePage } from '../pages/welcome/welcome';
     WelcomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [Http]
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
