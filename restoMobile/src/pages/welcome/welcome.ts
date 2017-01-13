98d7d1ea2d132c50dd9eb9a8ff2772e80f09573c
Tex08 <alex@pelletier.com>
2017-01-13 15:54:28 -0500

    Added a language select and continue button. #24

--- restoMobile/src/pages/welcome/welcome.ts
+++ restoMobile/src/pages/welcome/welcome.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+
+//Reminder import NavParams
 
 @Component({
   selector: 'page-welcome',
   templateUrl: 'welcome.html'
 })
 export class WelcomePage {
+	language: string = "Hello";
 
   constructor(public navCtrl: NavController) {
     
