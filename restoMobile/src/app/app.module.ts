3eb72061cfbd8defbfc59dfb9e969a716d60199d
Tex08 <alex@pelletier.com>
2017-01-13 15:07:47 -0500

    Created page that will be used as a welcome screen. #24

--- restoMobile/src/app/app.module.ts
+++ restoMobile/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { WelcomePage } from '../pages/welcome/welcome';
 
 @NgModule({
   declarations: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    WelcomePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -17,7 +19,8 @@ import { Page2 } from '../pages/page2/page2';
   entryComponents: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    WelcomePage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
---
+++ restoMobile/src/pages/welcome/welcome.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html'
+})
+export class WelcomePage {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
