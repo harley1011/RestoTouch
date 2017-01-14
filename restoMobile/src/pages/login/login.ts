3c318b7bf40272683ae455864082376b3199190d
harley1011 <harley.1011@gmail.com>
2017-01-14 18:09:47 -0500

    Removing unused imports and comments

--- restoMobile/src/pages/login/login.ts
+++ restoMobile/src/pages/login/login.ts
@@ -1,37 +1,25 @@
-import { Component, Injectable } from '@angular/core';
-import {Http, Response} from '@angular/http';
-import { NavController, NavParams } from 'ionic-angular';
-import { AuthService } from '../services/auth.service';
-import { User } from '../shared/models/user';
-import { RestaurantListPage } from '../restaurant-list/restaurant-list';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {AuthService} from '../services/auth.service';
+import {User} from '../shared/models/user';
+import {RestaurantListPage} from '../restaurant-list/restaurant-list';
 
-/*
-  Generated class for the Login page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-    user = new User('', '');
-    errorMessage: string;
+  user = new User('', '');
+  errorMessage: string;
 
   constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public authService: AuthService) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-
-  }
+              public authService: AuthService) {
+  }n
 
   onSubmit() {
-     this.authService.authenticateUser(this.user)
-       .subscribe(generalResponse =>
-           this.navCtrl.push(RestaurantListPage)
-         , error => this.errorMessage = error);
+    this.authService.authenticateUser(this.user)
+      .subscribe(generalResponse =>
+          this.navCtrl.push(RestaurantListPage)
+        , error => this.errorMessage = error);
   }
 }
