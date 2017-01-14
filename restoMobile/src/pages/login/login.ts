0ff174f51ccff13a5385eee6affe68937531efbd
Hilary Chan <hilarychan079@gmail.com>
2017-01-14 18:07:04 -0500

    cleaned code

--- restoMobile/src/pages/login/login.ts
+++ restoMobile/src/pages/login/login.ts
@@ -3,8 +3,6 @@ import {Http, Response} from '@angular/http';
 import { NavController, NavParams } from 'ionic-angular';
 import { AuthService } from '../services/auth.service';
 import { User } from '../shared/models/user';
-//import {ApiEndpointService} from '../services/api-endpoint.service';
-import { Page2 } from '../page2/page2';
 import { RestaurantListPage } from '../restaurant-list/restaurant-list';
 
 /*
@@ -23,10 +21,7 @@ export class LoginPage {
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public authService: AuthService) {
-    //this.user = new User('', '', '', '', '');
-    //this.errorMessage = '';
-  }
+              public authService: AuthService) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
