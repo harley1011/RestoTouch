f8b00253a6645b6a2b63973c4cb8e006608e6afe
Samer El-Achkar <samerelachkarmail@gmail.com>
2017-01-15 00:13:23 -0500

    Menu List Page functionality completed #21

--- restoMobile/src/pages/menu-list/menu-list.ts
+++ restoMobile/src/pages/menu-list/menu-list.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { MenuPage } from '../menu/menu';
-import { MenuService } from '../services/menu.service';
-import { Menu } from '../../pages/shared/models/menu';
 
 @Component({
     selector: 'page-menu-list',
@@ -11,30 +9,16 @@ import { Menu } from '../../pages/shared/models/menu';
 
 export class MenuListPage {
     selectedMenu: any;
-    menus: Array<Menu>;
     selectedRestaurant: any;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, public menuService: MenuService) {
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
 
         this.selectedRestaurant = navParams.get('restaurant');
         this.selectedMenu = navParams.get('menu');
-        this.getMenus();
-
+        this.selectedRestaurant.Menus.forEach( menu => {
+            menu.selectedTranslation = menu.translations.find(translation => translation.languageCode == 'en');
+        })
     }
-
-    getMenus(): void {
-        this.menuService.getMenus().subscribe(
-            menus => {
-                this.menus = menus;
-                menus.forEach(menu => {
-                    menu.selectedTranslation = menu.translations.find(translation => translation.languageCode == 'en');
-                });
-            },
-            error => {
-                console.log(error);
-            }
-        );
-    };
 
     menuTapped(menu){
         this.navCtrl.push(MenuPage, {
