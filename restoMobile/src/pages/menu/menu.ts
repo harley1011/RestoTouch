67f5074e731c7a40fa352b376ca61706d5f07247
David Bastien <david.bastien5@gmail.com>
2017-01-23 15:15:55 -0500

    Change menu listing to custom objects for the purpose of this page #22

--- restoMobile/src/pages/menu/menu.ts
+++ restoMobile/src/pages/menu/menu.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Category } from '../shared/models/category';
 import { Item } from '../shared/models/items';
 import { Size } from '../shared/models/size';
+import { OrderableCategory, OrderableItem, OrderableSize } from './orderable-category';
 import { Menu } from '../shared/models/menu';
 import { CategoryService } from '../services/category.service';
 import { ItemService } from '../services/item.service';
@@ -16,6 +17,8 @@ export class MenuPage {
   selectedMenu: any;
   selectedLanguage: any;
   menu: Menu;
+  categories: Array<OrderableCategory>;
+  orders: Array<Item>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private categoryService: CategoryService,
@@ -24,6 +27,8 @@ export class MenuPage {
 
     this.selectedMenu = navParams.get('menu');
     this.selectedLanguage = navParams.get('language');
+    this.categories = [];
+    this.orders = [];
 
     this.getMenu(this.selectedMenu.id);
   }
@@ -41,8 +46,7 @@ export class MenuPage {
     this.menuService.getMenu(id).subscribe(
       menu => {
         this.menu = menu;
-        console.log(this.menu);
-        this.menu.categories.forEach( category => {
+        this.menu.categories.forEach(category => {
           category.selectedTranslation = category.translations.find(translation => translation.languageCode == this.selectedLanguage.languageCode);
           var item: Item;
           for (var i = 0; i < category.items.length; i++) {
@@ -54,10 +58,39 @@ export class MenuPage {
             }
           }
         });
+
+        this.initOrderableCategories();
       },
       error => {
         console.log(error);
       }
     );
+  }
+
+  initOrderableCategories(): void {
+    var self = this;
+    let orderableCategory: OrderableCategory;
+    let orderableItem: OrderableItem;
+    let orderableSize: OrderableSize;
+    this.menu.categories.forEach(category => {
+      orderableCategory = new OrderableCategory(category, []);
+      category.items.forEach(item => {
+        orderableItem = new OrderableItem(item, []);
+        item.sizes.forEach(size => {
+          orderableSize = new OrderableSize(size, 0);
+          orderableItem.sizes.push(orderableSize);
+        });
+        orderableCategory.items.push(orderableItem);
+      });
+      self.categories.push(orderableCategory);
+    });
+  }
+
+  addOrder(category: OrderableCategory, item: OrderableItem, size: OrderableSize): void {
+    console.log('add');
+  }
+
+  removeOrder(category: OrderableCategory, item: OrderableItem, size: OrderableSize): void {
+    console.log('remove');
   }
 }
