c210c081bed4c375c9ad3230ab15ce65c9261251
Tex08 <alex@pelletier.com>
2017-01-23 23:26:39 -0500

    #18 Added edit mode to profile page.

--- restoWeb/src/client/app/dashboard/profile/profile.component.ts
+++ restoWeb/src/client/app/dashboard/profile/profile.component.ts
@@ -11,11 +11,17 @@ import {User} from '../../shared/models/user';
 
 export class ProfileComponent {
 	user = new User('', '');
+	editing: boolean;
 
 	constructor(private profileService: ProfileService) {
 		this.profileService.getProfile().subscribe(user => {
 			this.user = user;
 			console.log(user);
 		});
+		this.editing = false;
+	}
+
+	edit() {
+		this.editing = !this.editing;
 	}
 }
--- restoWeb/src/client/app/dashboard/profile/profile.module.ts
+++ restoWeb/src/client/app/dashboard/profile/profile.module.ts
@@ -1,8 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProfileComponent} from './profile.component';
+import {FormsModule} from '@angular/forms';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   declarations: [ProfileComponent],
   exports: [ProfileComponent]
 })
