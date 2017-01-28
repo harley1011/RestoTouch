5d297ef79b0994be84b88d8347f45c72fe2bafad
Samer El-Achkar <samerelachkarmail@gmail.com>
2017-01-27 20:56:45 -0500

    Setting up e2e testing for ionic2(restoMobile) #101

---
+++ restoMobile/e2e/login.e2e-spec.ts
@@ -0,0 +1,27 @@
+//Reference for writing tests: http://www.protractortest.org/#/api
+import { browser, by, element } from 'protractor';
+//import { LoginPage } from '../pages/login/login';
+
+describe("Dummy Test Case", () => {
+  beforeEach(() => {
+    browser.get('');
+  });
+
+  it('should pass!', () => {
+    expect('test').toEqual('test');
+  });
+
+});
+
+
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* */
