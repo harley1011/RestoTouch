4e211dc83a87c0534d18d0a11418bc734cfd4ad2
Samer El-Achkar <samerelachkarmail@gmail.com>
2017-01-30 20:09:28 -0500

    Updating ionic version #102

--- restoMobile/protractor.conf.js
+++ restoMobile/protractor.conf.js
@@ -2,34 +2,33 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 /*global jasmine */
-//var SpecReporter = require('jasmine-spec-reporter');
-var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+var SpecReporter = require('jasmine-spec-reporter');
 
 exports.config = {
-    allScriptsTimeout: 11000,
-    specs: [
-          'e2e/*.e2e-spec.ts'
-    ],
-    capabilities: {
-        'browserName': 'chrome'
-    },
-    directConnect: true,
-    baseUrl: 'http://localhost:8100/',
-    framework: 'jasmine',
-    jasmineNodeOpts: {
-        showColors: true,
-        defaultTimeoutInterval: 30000,
-        print: function() {}
-    },
-    useAllAngular2AppRoots: true,
-    beforeLaunch: function() {
-        require('ts-node').register({
-            project: 'e2e'
-        });
+  allScriptsTimeout: 11000,
+  specs: [
+    './e2e/**/*.e2e-spec.ts'
+  ],
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  directConnect: true,
+  baseUrl: 'http://localhost:8100/',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {}
+  },
+  useAllAngular2AppRoots: true,
+  beforeLaunch: function() {
+    require('ts-node').register({
+      project: 'e2e'
+    });
 
-        require('connect')().use(require('serve-static')('www')).listen(8100);
-    },
-    onPrepare: function() {
-        jasmine.getEnv().addReporter(new SpecReporter());
-    }
+    require('connect')().use(require('serve-static')('www')).listen(8100);
+  },
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(new SpecReporter());
+  }
 };
