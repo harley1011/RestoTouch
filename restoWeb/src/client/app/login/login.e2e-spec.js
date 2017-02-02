2826f612f7f4da871f34b034f780b3aeffe11348
Tex08 <alex@pelletier.com>
2017-02-01 19:19:59 -0500

    #102 e2e test for creating a valid restaurant.

--- restoWeb/src/client/app/login/login.e2e-spec.js
+++ restoWeb/src/client/app/login/login.e2e-spec.js
@@ -6,7 +6,7 @@ describe("TEST CASES FOR LOGIN PAGE", function () {
     browser.get('http://localhost:5555/');
   });
 
-  it("Correct text display of the tag element title", function () {
+  it("Display the correct text of the tag element title", function () {
     var loginTitle = browser.getTitle();
     expect(loginTitle).toEqual("RestoTouch");
   });
@@ -22,6 +22,24 @@ describe("TEST CASES FOR LOGIN PAGE", function () {
     expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'dashboard/home');
 
   });
+
+  it("Navigate to the restaurant page", function () {
+    browser.get(browser.baseUrl + 'dashboard/restaurants');
+    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'dashboard/restaurants');
+  });
+
+  it("Create a valid restaurant", function () {
+    browser.get(browser.baseUrl + 'dashboard/restaurants');
+    var addRestoBtn = element(by.id('addRestoBtn'));
+    addRestoBtn.click();
+    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'dashboard/restaurant');
+    element(by.id("name")).sendKeys("restoName");
+    element(by.id("description")).sendKeys("Restaurant description");
+    element(by.id("address")).sendKeys("540 Test Address");
+    var createrestoBtn = element(by.id('createRestoBtn'));
+    createrestoBtn.click();
+    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'dashboard/restaurants');
+  })
 
 });
 
