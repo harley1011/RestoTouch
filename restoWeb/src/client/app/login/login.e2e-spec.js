8eb0bf989e08ae9758bbe98919039f100f846e34
Tex08 <alex@pelletier.com>
2017-02-01 21:17:44 -0500

    #102 e2e test for creating a valid order item.

--- restoWeb/src/client/app/login/login.e2e-spec.js
+++ restoWeb/src/client/app/login/login.e2e-spec.js
@@ -1,12 +1,12 @@
 //Reference for writing tests: http://www.protractortest.org/#/api
 
-describe("TEST CASES FOR LOGIN PAGE", function () {
+describe("RESTOTOUCH TEST CASES", function () {
 
   beforeEach(function () {
     browser.get('http://localhost:5555/');
   });
 
-  it("Display the correct text of the tag element title", function () {
+  it("Display the correct text of the title on the welcome page", function () {
     var loginTitle = browser.getTitle();
     expect(loginTitle).toEqual("RestoTouch");
   });
@@ -26,20 +26,62 @@ describe("TEST CASES FOR LOGIN PAGE", function () {
   it("Navigate to the restaurant page", function () {
     browser.get(browser.baseUrl + 'dashboard/restaurants');
     expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'dashboard/restaurants');
+    //expect(browser.isElementPresent(element(by.className('card-block')))).toBe(false);
   });
 
   it("Create a valid restaurant", function () {
     browser.get(browser.baseUrl + 'dashboard/restaurants');
     var addRestoBtn = element(by.id('addRestoBtn'));
     addRestoBtn.click();
+
+    //Enter restaurant information
     expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'dashboard/restaurant');
     element(by.id("name")).sendKeys("restoName");
     element(by.id("description")).sendKeys("Restaurant description");
     element(by.id("address")).sendKeys("540 Test Address");
-    var createrestoBtn = element(by.id('createRestoBtn'));
-    createrestoBtn.click();
+
+    //Click on the create restaurant button
+    var createRestoBtn = element(by.id('createRestoBtn'));
+    createRestoBtn.click();
+
+    //Verify that user gets redirected to correct page, and that an element (the 
+    //created restaurant) is present on the restaurant-list page.
     expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'dashboard/restaurants');
+    expect(browser.isElementPresent(element(by.className('card-block')))).toBe(true);
   })
+
+  it("Create a valid order item", function () {
+    browser.get(browser.baseUrl + 'dashboard/items');
+    var addItemBtn = element(by.id('addItemBtn'));
+    addItemBtn.click();
+    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'dashboard/item');
+
+    //Enter valid item information
+    element(by.id("name")).sendKeys("Item Name");
+    element(by.id("description")).sendKeys("Short item description");
+    element(by.name("sizeName")).sendKeys("TestSize");
+    element(by.id("price")).sendKeys(5);
+
+    //Add a valid item size
+    var addSizeBtn = element(by.id('addSizeBtn'));
+    addSizeBtn.click();
+
+    //Add a valid ingredient group
+    var addIngredientGroupBtn = element(by.id('addIngredientGroupBtn'));
+    addIngredientGroupBtn.click();
+    element(by.name("ingredientGroupName")).sendKeys("Ingredient group test");
+    var confirmIngredientGroupBtn = element(by.id('confirmIngredientGroupBtn'));
+    confirmIngredientGroupBtn.click();
+
+    //Click on the create restaurant button
+    var createItemBtn = element(by.id('createItemBtn'));
+    createItemBtn.click();
+
+    //Verify that user gets redirected to correct page, and that an element (the 
+    //created restaurant) is present on the restaurant-list page.
+    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'dashboard/items');
+    expect(browser.isElementPresent(element(by.className('card-block')))).toBe(true);
+  });
 
 });
 
