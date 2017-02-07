262a478bc235203fa4dd3a9ba422c753e3d910fb
Tex08 <alex@pelletier.com>
2017-02-07 01:23:20 -0500

    #33 Temp list setup and orders attribute added.

--- restoWeb/src/client/app/dashboard/unpaid-orders/unpaid-orders.component.ts
+++ restoWeb/src/client/app/dashboard/unpaid-orders/unpaid-orders.component.ts
@@ -1,11 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Order} from '../../shared/models/order';
+import {ItemService} from '../item/item.service';
+import {Item} from './../../shared/models/items';
 
 
 @Component({
   moduleId: module.id,
   selector: 'unpaid-orders-cmp',
   templateUrl: 'unpaid-orders.component.html',
-  providers: []
+  providers: [ItemService]
 })
 
-export class UnpaidOrdersComponent {}
+export class UnpaidOrdersComponent implements OnInit {
+	orders: Array<Order>;
+	items: Array<Item>;
+
+	constructor(private itemService: ItemService) {
+	}
+
+	ngOnInit(): void {
+    this.itemService.getItems().subscribe(items => {
+        this.items = items;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+}
