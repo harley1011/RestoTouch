56273f586b25a53cf31c96fd8d7293c5f9ff20b1
Tex08 <alex@pelletier.com>
2017-03-04 16:38:59 -0500

    #32 Routing to unpaid orders page when selecting cashier mode.

--- restoWeb/src/client/app/dashboard/kitchen-cashier-select/kitchen-cashier-select.component.ts
+++ restoWeb/src/client/app/dashboard/kitchen-cashier-select/kitchen-cashier-select.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from 'ng2-translate';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   moduleId: module.id,
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
   providers: []
 })
 
-export class KitchenCashierSelectComponent {
+export class KitchenCashierSelectComponent implements OnInit {
 
+	id: number;
+
+	constructor(private router: Router,
+				private route: ActivatedRoute,
+				private translate: TranslateService) {
+
+	}
+
+	ngOnInit() : void {
+		this.route.params.forEach((params: Params) => {
+        if (params['id']) {
+        	this.id = params['id'];
+        }
+      });
+	}
+
+	kitchenSelect(): void {
+      this.router.navigate(['/dashboard/unpaidOrders', this.id]);
+  	}
+
+  	cashierSelect(): void {
+      this.router.navigate(['/dashboard/unpaidOrders', this.id]);
+  	}
 }
--- restoWeb/src/client/app/dashboard/kitchen-cashier-select/kitchen-cashier-select.module.ts
+++ restoWeb/src/client/app/dashboard/kitchen-cashier-select/kitchen-cashier-select.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {TranslateModule} from 'ng2-translate';
 import {KitchenCashierSelectComponent} from './kitchen-cashier-select.component';
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   declarations: [KitchenCashierSelectComponent],
   exports: [KitchenCashierSelectComponent]
 })
