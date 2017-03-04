f4bf726a00bc050e2c4aeb09116d0c9d5c7c67f6
Tex08 <alex@pelletier.com>
2017-03-04 17:04:02 -0500

    #32 Kitchen page fetches new orders from cache.

--- restoWeb/src/client/app/dashboard/kitchen/kitchen.component.ts
+++ restoWeb/src/client/app/dashboard/kitchen/kitchen.component.ts
@@ -1,29 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from 'ng2-translate';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { TranslateService } from 'ng2-translate';
+import { OrderService } from '../../services/order.service';
+import { OrderNotifierService } from '../../services/order-notifier.service';
+import { Order } from '../../shared/models/order';
+
+import { TranslationSelectComponent } from '../../shared/translation-select/translation-select.component';
+import { Language } from './../../shared/models/language';
 
 @Component({
   moduleId: module.id,
   selector: 'kitchen-cmp',
   templateUrl: 'kitchen.component.html',
-  providers: []
+  providers: [OrderService, OrderNotifierService]
 })
 
 export class KitchenComponent implements OnInit {
 
-	id: number;
+  orders: Array<Order> = [];
+  order: Order;
+  id: number;
 
-	constructor(private router: Router,
-				private route: ActivatedRoute,
-				private translate: TranslateService) {
+  @ViewChild(TranslationSelectComponent)
+  private translationSelectComponent: TranslationSelectComponent;
+
+	constructor(private orderService: OrderService,
+              private orderNotifierService: OrderNotifierService,
+				      private route: ActivatedRoute,
+				      private translate: TranslateService) {
 
 	}
 
-	ngOnInit() : void {
-		this.route.params.forEach((params: Params) => {
+	ngOnInit(): void {
+    this.route.params.forEach((params: Params) => {
         if (params['id']) {
-        	this.id = params['id'];
+          this.id = params['id'];
+          this.orderNotifierService.connectToOrderNotifier(this.id).subscribe((order: any) => {
+            this.order = JSON.parse(order);
+            console.log(this.order);
+            this.order.orderedItems.forEach(orderedItem => {
+              orderedItem.item.selectedTranslation = orderedItem.item.translations.find(translation => translation.languageCode === this.translationSelectComponent.selectedLanguage.languageCode);
+            });
+            this.orders.push(this.order);
+          });
+          /* Not working, formatting of returned orders isn't matching the order model
+
+          this.orderService.retrieveOrders(this.id).subscribe((orders: any) => {
+            console.log(orders);
+            //this.orders = JSON.parse(orders);
+            
+            this.order.orderedItems.forEach(orderedItem => {
+              orderedItem.item.selectedTranslation = orderedItem.item.translations.find(translation => translation.languageCode === this.translationSelectComponent.selectedLanguage.languageCode);
+            });
+          })
+          */
         }
     });
-	}
+  }
+
+  onSelectLanguage(language: Language) {
+    this.orders.forEach(order => {
+      order.orderedItems.forEach(orderedItem => {
+        orderedItem.item.selectedTranslation = orderedItem.item.translations.find(translation => translation.languageCode === language.languageCode);
+      });
+    });
+  }
+    
 }
--- restoWeb/src/client/app/dashboard/kitchen/kitchen.module.ts
+++ restoWeb/src/client/app/dashboard/kitchen/kitchen.module.ts
@@ -1,13 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TranslateModule} from 'ng2-translate';
+import {TranslationSelectModule} from '../../shared/translation-select/translation-select.module';
+import {LanguageService} from '../../services/language.service';
 import {KitchenComponent} from './kitchen.component';
 @NgModule({
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, TranslationSelectModule],
   declarations: [KitchenComponent],
   exports: [KitchenComponent]
 })
 
 export class KitchenModule {
-
+	constructor(private languageService: LanguageService) {
+  }
 }
