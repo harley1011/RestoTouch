da37c6d7edd17e512ac8732ff24a6808bb0bc02c
Tamy <thuynhse@outlook.com>
2017-03-18 13:40:33 -0400

    wrote unit test for order() #11

--- restoMobile/src/pages/menu/menu.spec.ts
+++ restoMobile/src/pages/menu/menu.spec.ts
@@ -25,6 +25,28 @@ describe('Pages: MenuPage', () => {
     expect(instance).not.toBeNull();
   });
 
+  // testing condition branches of method order()
+  // pass if condition
+  it('should call method "presentPrompt" for "na" notification', () => {
+    spyOn(instance, 'presentPrompt');
+    spyOn(instance, 'notifyAtEndOrder');
+    instance.selectedRestaurant.orderNotiFlag = "na";
+    instance.order();
+    expect(instance.presentPrompt).toHaveBeenCalled();
+    expect(instance.notifyAtEndOrder).not.toHaveBeenCalled();
+  });
+  // skip if condition to else body
+  it('should NOT call method "presentPrompt" for other notifications not "na"', () => {
+    spyOn(instance, 'presentPrompt');
+    spyOn(instance, 'notifyAtEndOrder');
+    spyOn(instance, 'sendOrder');
+    instance.selectedRestaurant.orderNotiFlag = "nu";
+    instance.order();
+    expect(instance.presentPrompt).not.toHaveBeenCalled();
+    expect(instance.notifyAtEndOrder).toHaveBeenCalled();
+    expect(instance.sendOrder).toHaveBeenCalled();
+  });
+
 });
 
 
