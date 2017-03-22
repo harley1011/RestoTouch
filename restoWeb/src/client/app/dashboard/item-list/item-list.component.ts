984b044b63e03c29e3ebdcdd6501bb1c48365ee5
David Bastien <david.bastien5@gmail.com>
2017-03-22 15:28:57 -0400

    #132 working on sorting

--- restoWeb/src/client/app/dashboard/item-list/item-list.component.ts
+++ restoWeb/src/client/app/dashboard/item-list/item-list.component.ts
@@ -13,11 +13,13 @@ import {TranslationSelectComponent} from '../../shared/translation-select/transl
 
 export class ItemListComponent implements OnInit {
   items: Array<Item>;
+  sortAscending: boolean;
 
   @ViewChild(TranslationSelectComponent)
   private translationSelectComponent: TranslationSelectComponent;
 
   constructor(private itemService: ItemService, private router: Router) {
+    this.sortAscending = false;
   }
 
   ngOnInit(): void {
@@ -46,4 +48,33 @@ export class ItemListComponent implements OnInit {
   modify(item: Item): void {
     this.router.navigate(['/dashboard/item', item.id]);
   }
+
+  sortItems(): void {
+    this.sortAscending = !this.sortAscending;
+    if (this.sortAscending) {
+      this.items.sort(compareItemAscending);
+    } else {
+      this.items.sort(compareItemDescending);
+    }
+  }
+}
+
+function compareItemAscending (item1: Item, item2: Item) {
+  if (item1.selectedTranslation.name < item2.selectedTranslation.name) {
+    return -1;
+  } else if (item1.selectedTranslation.name > item2.selectedTranslation.name) {
+    return 1;
+	} else {
+		return 0;
+	}
+}
+
+function compareItemDescending (item1: Item, item2: Item) {
+  if (item1.selectedTranslation.name > item2.selectedTranslation.name) {
+    return -1;
+  } else if (item1.selectedTranslation.name < item2.selectedTranslation.name) {
+    return 1;
+	} else {
+		return 0;
+	}
 }
