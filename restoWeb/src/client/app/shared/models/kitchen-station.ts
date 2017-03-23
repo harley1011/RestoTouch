32079f79621ace25de3ea931250e2099ae08b833
Tamy <thuynhse@outlook.com>
2017-03-23 19:36:45 -0400

    #134 updated kitchen-station model, updated swagger

--- restoWeb/src/client/app/shared/models/kitchen-station.ts
+++ restoWeb/src/client/app/shared/models/kitchen-station.ts
@@ -5,7 +5,7 @@ export class KitchenStation {
   constructor (
     public translations: Array<KitchenTranslations>,
     public selectedTranslation: KitchenTranslations,
-    public categories: Array<Category>,
+    public kitCat: Array<Category>,
     public id?: number
   ) { }
 }
