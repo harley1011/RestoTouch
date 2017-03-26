b2a87788db249fb2266d2fdd2174497362540e59
Tamy <thuynhse@outlook.com>
2017-03-25 22:52:32 -0400

    #134 kitchen mode: each station now show their assigned items

--- restoWeb/src/client/app/dashboard/kitchen/kitchen.component.ts
+++ restoWeb/src/client/app/dashboard/kitchen/kitchen.component.ts
@@ -27,9 +27,11 @@ export class KitchenComponent implements OnInit {
   restoMode: string;
   restaurant: Restaurant;
   selectedStationInfo: [string, boolean] = ['', false];
+  selectedStationId: number;
   stationList: Array<KitchenStation> = [];
   stationItemResponsability: Array<Item> = [];
-  orderResponsibilityToSelectedStation: Order;
+  assignedOrder: Order;
+  listOfAssignedOrders: Array<Order>;
 
   @ViewChild(TranslationSelectComponent)
   private translationSelectComponent: TranslationSelectComponent;
@@ -51,23 +53,9 @@ export class KitchenComponent implements OnInit {
             this.restoMode = restaurant.kitCashModeFlag;
             this.restaurant = restaurant;
             this.stationList = restaurant.kitchenStations;
-          })
-          this.orderNotifierService.connectToOrderNotifier(this.id).subscribe((order: any) => {
-            this.order = JSON.parse(order);
-            console.log(this.order);
-            this.order.orderedItems.forEach(orderedItem => {
-              orderedItem.item.selectedTranslation = orderedItem.item.translations.find(translation => translation.languageCode === this.translationSelectComponent.selectedLanguage.languageCode);
-            });
-            this.orders.push(this.order);
           });
+          this.getOrders(this.id);
 
-
-           //Get previously cached orders
-           this.orderService.retrieveOrders(this.id).subscribe(orders => {
-           this.orders = orders;
-           console.warn(this.orders);
-
-           });
 
         }
     });
@@ -86,24 +74,63 @@ export class KitchenComponent implements OnInit {
           //2
           ingredientGroup.ingredients.forEach(ingredient => {
             ingredient.selectedTranslation = ingredient.translations.find(translation => translation.languageCode === language.languageCode);
-          })
-        })
+          });
+        });
         //B
         orderedItem.sizes.forEach( size => {
           size.size.selectedTranslation = size.size.translations.find(translation => translation.languageCode === language.languageCode);
-        })
+        });
 
       });
     });
   }
 
-  stationSelect(id: string, i: number): void {
-    this.selectedStationInfo = [id, true];
+  getOrders(id: number):void {
+          this.orderNotifierService.connectToOrderNotifier(id).subscribe((order: any) => {
+            this.order = JSON.parse(order);
+            this.order.orderedItems.forEach(orderedItem => {
+              orderedItem.item.selectedTranslation = orderedItem.item.translations.find(translation => translation.languageCode === this.translationSelectComponent.selectedLanguage.languageCode);
+            });
+            this.orders.push(this.order);
+          });
+
+           //Get previously cached orders
+           this.orderService.retrieveOrders(id).subscribe(orders => {
+            this.orders = orders;
+            this.listOfAssignedOrders = orders;
+           });
+  }
+
+  stationSelect(id: number, i: number): void {
+    this.selectedStationId = id;
+    this.selectedStationInfo = [this.restaurant.kitchenStations[i].name, true];
     this.stationItemResponsability = this.restaurant.kitchenStations[i].kitItem;
+
+    this.listOfAssignedOrders.forEach(order => {
+      this.filterToThisStation(order);
+      console.log(this.assignedOrder);
+      //this.listOfAssignedOrders.push(this.assignedOrder);
+    });
   }
 
   goBack(): void {
+    this.getOrders(this.id);
     this.selectedStationInfo[1] = false;
   }
 
+  completeOrder(o: Order): void {
+
+  }
+
+  filterToThisStation(o: Order): void {
+    let keepItem: Order = new Order([], null,'','');
+    o.orderedItems.forEach(item => {
+      this.stationItemResponsability.forEach(stationItem=> {
+        if(item.item.id === stationItem.id) {
+          keepItem.orderedItems.push(item);
+        }
+      });
+    });
+    o.orderedItems = keepItem.orderedItems;
+  }
 }
