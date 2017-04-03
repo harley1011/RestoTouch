281d7fb8c983794fbf9c9266d42d7e800023e963
harley1011 <harley.1011@gmail.com>
2017-04-03 00:17:26 -0400

    Adding back app.js for swagger doc viewing and adding production files

---
+++ restoWeb/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var morgan = require('morgan');
+var multer = require('multer');
+var fs = require('fs');
+var orderNotifier = require('./restoWeb/api/controllers/orderNotifier.js');
+
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname + "/restoWeb" // required config
+};
+
+
+app.use(cors());
+app.use(morgan('combined'));
+
+require('./restoWeb/websiteRoutes')(app, express);
+
+app.all('/*', [require('./restoWeb/authenticator.js')]);
+
+var port = process.env.PORT || 10010;
+var appListen = app.listen(port);
+SwaggerExpress.create(config, function (err, swaggerExpress) {
+  if (err) {
+    throw err;
+  }
+
+  swaggerExpress.register(app);
+
+  orderNotifier(appListen);
+  console.log('Server listening on port', port);
+});
+
